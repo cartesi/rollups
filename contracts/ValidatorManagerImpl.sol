00be76e1e2480f1bfcd1e6054d487825bdcebb2e
felipeargento <felipe@cartesi.io>
2021-02-04 17:34:48 +0800

    Implement interface

--- contracts/ValidatorManagerImpl.sol
+++ contracts/ValidatorManagerImpl.sol
@@ -20,16 +20,15 @@
 // be used independently under the Apache v2 license. After this component is
 // rewritten, the entire component will be released under the Apache v2 license.
 
-
 /// @title Validator Manager Implementation
 pragma solidity ^0.7.0;
 
-import "./ValidatorManager.sol"
+import "./ValidatorManager.sol";
 
 contract ValidatorManagerImpl is ValidatorManager {
     address immutable descartesV2; // descartes 2 contract using this validator
     bytes32 currentClaim; // current claim - first claim of this epoch
-    address[] validators; // current validators
+    address payable[] validators; // current validators
 
     // A bit set for each validator that agrees with current claim,
     // on their respective positions
@@ -38,7 +37,6 @@ contract ValidatorManagerImpl is ValidatorManager {
     // Every validator who should approve (in order to reach consensus) will have a one set on this mask
     // This mask is updated if a validator is added or removed
     uint32 consensusGoalMask;
-
 
     // @notice functions modified by onlyDescartesV2 will only be executed if
     // they're called by DescartesV2 contract, otherwise it will throw an exception
@@ -53,7 +51,7 @@ contract ValidatorManagerImpl is ValidatorManager {
     // @notice populates validators array and creates a consensus mask
     // @params _descartesV2 address of descartes contract
     // @params _validators initial validator set
-    constructor(address _descartesV2, address[] _validators) {
+    constructor(address _descartesV2, address payable[] memory _validators) {
         descartesV2 = _descartesV2;
         validators = _validators;
 
@@ -72,37 +70,50 @@ contract ValidatorManagerImpl is ValidatorManager {
     // @return [claimer1, claimer2] if there is  Conflcit
     //         [claimer1, address(0)] if there is Consensus
     //         [address(0), address(0)] if there is NoConflcit
-    function onClaim(
-        address _sender,
-        bytes32 _claim
-    )
-    public
-    onlyDescartesV2
-    returns (
-        Result,
-        bytes32[2] claims,
-        address[2] claimers
-    )
+    function onClaim(address payable _sender, bytes32 _claim)
+        public
+        override
+        onlyDescartesV2
+        returns (
+            Result,
+            bytes32[2] memory,
+            address payable[2] memory
+        )
     {
-        require(_claim != bytes32(0), "claim of bytes32(0) is invalid")
-        // TODO: should claims by non validators just revert?
-        if (!isAllowed(_sender)) return (Result.NoConflict, new bytes32[2], new address[2];
+        require(_claim != bytes32(0), "claim of bytes32(0) is invalid");
 
-        if (claim == bytes32(0)) {
-            claim = _claim;
+        // TODO: should claims by non validators just revert?
+        if (!isAllowed(_sender))
+            return (
+                Result.NoConflict,
+                [bytes32(0), bytes32(0)],
+                [payable(0), payable(0)]
+            );
+
+        if (currentClaim == bytes32(0)) {
+            currentClaim = _claim;
         }
 
-        if (_claim != claim) {
+        if (_claim != currentClaim) {
             return (
                 Result.Conflict,
-                bytes32[claim, _claim],
-                address[popClaimer(), _sender]
+                [currentClaim, _claim],
+                [getClaimerOfCurrentClaim(), _sender]
             );
         }
 
-        return updateClaimAgreementMask(_sender) ?
-            (Result.Consensus, bytes32[_winningClaim, bytes32(0)], address[_winner, address(0)]) :
-            (Result.NoConflict, bytes32[bytes32(0), bytes32(0)], address[address(0), address(0)]); 
+        return
+            isConsensus(updateClaimAgreementMask(_sender), consensusGoalMask)
+                ? (
+                    Result.Consensus,
+                    [_claim, bytes32(0)],
+                    [_sender, payable(0)]
+                )
+                : (
+                    Result.NoConflict,
+                    [bytes32(0), bytes32(0)],
+                    [payable(0), payable(0)]
+                );
     }
 
     // @notice called when a dispute ends in descartesv2
@@ -110,26 +121,39 @@ contract ValidatorManagerImpl is ValidatorManager {
     // @params _loser address of dispute loser
     // @returns result of dispute being finished
     function onDisputeEnd(
-        address _winner,
-        address _loser,
+        address payable _winner,
+        address payable _loser,
         bytes32 _winningClaim
     )
-    onlyDescartesV2
-    public
-    returns (
-        Result,
-        bytes32[2],
-        address[2]
-    )
+        public
+        override
+        onlyDescartesV2
+        returns (
+            Result,
+            bytes32[2] memory,
+            address payable[2] memory
+        )
     {
         // remove validator also removes validator from both bitmask
-        (claimAgreementMask, consensusGoalMask) = removeFromValidatorSetAndBothBitmasks(_loser);
+        (
+            claimAgreementMask,
+            consensusGoalMask
+        ) = removeFromValidatorSetAndBothBitmasks(_loser);
 
         if (_winningClaim == currentClaim) {
             // first claim stood, dont need to update the bitmask
-            return claimAgreementMask == consensusGoalMask ?
-                (Result.Consensus, bytes32[_winningClaim, bytes32(0)], address[_winner, address(0)]) :
-                (Result.NoConflict, bytes32[bytes32(0), bytes32(0)], address[address(0), address(0)]); 
+            return
+                isConsensus(claimAgreementMask, consensusGoalMask)
+                    ? (
+                        Result.Consensus,
+                        [_winningClaim, bytes32(0)],
+                        [_winner, payable(0)]
+                    )
+                    : (
+                        Result.NoConflict,
+                        [bytes32(0), bytes32(0)],
+                        [payable(0), payable(0)]
+                    );
         }
 
         // if first claim lost, and other validators have agreed with it
@@ -137,20 +161,29 @@ contract ValidatorManagerImpl is ValidatorManager {
         if (claimAgreementMask != 0) {
             return (
                 Result.Conflict,
-                bytes32[claim, _winningClaim],
-                address[popClaimer(), _winner]
+                [currentClaim, _winningClaim],
+                [getClaimerOfCurrentClaim(), _winner]
             );
         }
         // else there are no valdiators that agree with losing claim
         // but we check for consensus in case the winner is the only validator left
-        return updateClaimAgreementMask(_winner) ?
-            (Result.Consensus, bytes32[_winningClaim, bytes32(0)], address[_winner, address(0)]) :
-            (Result.NoConflict, bytes32[bytes32(0), bytes32(0)], address[address(0), address(0)]); 
+        return
+            isConsensus(updateClaimAgreementMask(_winner), consensusGoalMask)
+                ? (
+                    Result.Consensus,
+                    [_winningClaim, bytes32(0)],
+                    [_winner, payable(0)]
+                )
+                : (
+                    Result.NoConflict,
+                    [bytes32(0), bytes32(0)],
+                    [payable(0), payable(0)]
+                );
     }
 
     // @notice called when a new epoch starts
     // @return current claim
-    function onNewEpoch() public onlyDescartesV2 returns (bytes32) {
+    function onNewEpoch() public override onlyDescartesV2 returns (bytes32) {
         bytes32 tmpClaim = currentClaim;
 
         // clear current claim
@@ -165,35 +198,41 @@ contract ValidatorManagerImpl is ValidatorManager {
 
     // @notice get one of the validators that agreed with current claim
     // @return validator that agreed with current claim
-    function getClaimerOfCurrentClaim() internal returns (address) {
+    function getClaimerOfCurrentClaim()
+        internal
+        view
+        returns (address payable)
+    {
         require(
             claimAgreementMask != 0,
-             "No validators agree with current claim"
+            "No validators agree with current claim"
         );
 
         // TODO: we are always getting the first validator
         // on the array that agrees with the current claim to enter a dispute
         // should this be random?
-        for (uint i = 0; i < validators.length(); i++) {
-            if (validatorBitMask & (1 << i) == 1) {
-               return validators[i];
+        for (uint256 i = 0; i < validators.length; i++) {
+            if (claimAgreementMask & (1 << i) == 1) {
+                return validators[i];
             }
         }
+        revert("Agreeing validator not found");
     }
 
     // @notice updates the consensus goal mask
     // @return new consensus goal mask
-    function updateConsensusGoalMask() internal returns (uint32) {
+    function updateConsensusGoalMask() internal view returns (uint32) {
         // consensus goal is a number where
         // all bits related to validators are turned on
-        uint32 consensusMask = (2 ** validators.length) - 1;
+        uint32 consensusMask =
+            (uint32(2)**uint32(validators.length)) - uint32(1);
 
         // the optimistc assumption is that validators getting kicked out
         // a rare event. So we save gas by starting with the optimistic scenario
         // and turning the bits off for removed validators
-        for (uint i = 0; i < validators.length; i++) {
+        for (uint32 i = 0; i < validators.length; i++) {
             if (validators[i] == address(0)) {
-                uint32 zeroMask = ~(1 << i);
+                uint32 zeroMask = ~(uint32(1) << i);
                 consensusMask = consensusMask & zeroMask;
             }
         }
@@ -203,13 +242,18 @@ contract ValidatorManagerImpl is ValidatorManager {
     // @notice updates mask of validators that agreed with current claim
     // @params _sender address that of validator that will be included in mask
     // @return true if mask update led to consensus, false if not
-    function updateClaimAgreementMask(address _sender) internal returns (bool) {
-        for (uint i = 0; i < validators.length; i++) {
-            if (_sender == validators[i]) break;
+    function updateClaimAgreementMask(address _sender)
+        internal
+        returns (uint32)
+    {
+        for (uint32 i = 0; i < validators.length; i++) {
+            if (_sender == validators[i]) {
+                claimAgreementMask = claimAgreementMask | (uint32(1) << i);
+                break;
+            }
         }
-        claimAgreementMask = claimAgreementMask | (1 << i);
 
-        return claimAgreementMask == consensusGoalMask;
+        return claimAgreementMask;
     }
 
     // @notice removes a validator
@@ -217,26 +261,38 @@ contract ValidatorManagerImpl is ValidatorManager {
     // @returns new claim agreement bitmask
     // @returns new consensus goal bitmask
     function removeFromValidatorSetAndBothBitmasks(address _validator)
-    internal
-    returns (
-        uint32,
-        uint32
-    )
+        internal
+        returns (uint32, uint32)
     {
         uint32 newClaimAgreementMask;
         uint32 newConsensusGoalMask;
         // put address(0) in validators position
         // removes validator from claim agreement bitmask
         // removes validator from consensus goal mask
-        for (uint i = 0; i < validators.length; i++) {
+        for (uint32 i = 0; i < validators.length; i++) {
             if (_validator == validators[i]) {
                 validators[i] = address(0);
-                uint32 zeroMask = ~(1 << i);
+                uint32 zeroMask = ~(uint32(1) << i);
                 newClaimAgreementMask = claimAgreementMask & zeroMask;
                 newConsensusGoalMask = consensusGoalMask & zeroMask;
                 break;
             }
         }
-        return (newClaimAgreementMask, newConsensusMask);
+        return (newClaimAgreementMask, newConsensusGoalMask);
+    }
+
+    function isAllowed(address _sender) internal view returns (bool) {
+        for (uint256 i = 0; i < validators.length; i++) {
+            if (_sender == validators[i]) return true;
+        }
+        return false;
+    }
+
+    function isConsensus(uint32 _claimAgreementMask, uint32 _consensusGoalMask)
+        internal
+        pure
+        returns (bool)
+    {
+        return _claimAgreementMask == _consensusGoalMask;
     }
 }
