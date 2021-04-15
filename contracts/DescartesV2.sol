bf7f5fd2cf48f224df0ddcda5e55739e3b3b61a3
felipeargento <felipe@cartesi.io>
2021-04-15 17:41:53 +0800

    Transform descartesV2 in an interface

--- contracts/DescartesV2.sol
+++ contracts/DescartesV2.sol
@@ -20,148 +20,49 @@
 // be used independently under the Apache v2 license. After this component is
 // rewritten, the entire component will be released under the Apache v2 license.
 
-
 /// @title Interface DescartesV2 contract
 pragma solidity ^0.7.0;
 
-contract DescartesV2 {
+import "./Input.sol";
+import "./Output.sol";
+import "./ValidatorManager.sol";
+import "./DescartesV2.sol";
 
+import "@openzeppelin/contracts/math/SafeMath.sol";
 
+interface DescartesV2 {
+
+    // InputAccumulation - Inputs being accumulated for currrent epoch
+    // AwaitingConsensus - No disagreeing claims (or no claims)
+    // AwaitingDispute - Waiting for dispute to be over
     // inputs received during InputAccumulation will be included in the
     // current epoch. Inputs received while WaitingClaims or ChallengesInProgress
-    // are accumulated for N + 1
-    enum Phase { InputAccumulation, AwaitingConsensus, AwaitingDisputes }
-    /**
-    * // TODO: this is quite ugly, once agreed upon we can beautify it
-    *
-    *                All claims agreed OR challenge period ended
-    *                functions: claim() or finalizeEpoch()
-    *          +--------------------------------------------------+
-    *          |                                                  |
-    * +--------v-----------+   new input after IPAD     +---------+----------+
-    * |                    +--------------------------->+                    |
-    * | Input Accumulation |   firt claim after IPAD    | Awaiting Consensus |
-    * |                    +--------------------------->+                    |
-    * +-+------------------+                            +-----------------+--+
-    *   ^                                                                 ^  |
-    *   |                                              dispute resolved   |  |
-    *   |  dispute resolved                            before challenge   |  |
-    *   |  after challenge     +--------------------+  period ended       |  |
-    *   |  period ended        |                    +---------------------+  |
-    *   +----------------------+  Awaiting Dispute  |                        |
-    *                          |                    +<-----------------------+
-    *                          +--------------------+    conflicting claim
-    **/
+    // are accumulated for the next epoch
+    enum Phase {InputAccumulation, AwaitingConsensus, AwaitingDispute}
 
-    struct State {
-        bytes32 epochHash; // epoch hash to being suggested for epochs merkle tree
-        uint256 timestamp; // timestamp of claim submission
+    /// @notice claim the result of current epoch
+    /// @param _epochHash hash of epoch
+    /// @dev ValidatorManager makes sure that msg.sender is allowed
+    //       and that claim != bytes32(0)
+    /// TODO: add signatures for aggregated claims
+    function claim(bytes32 _epochHash) external;
 
-        bytes32 attestatorMask; // mask of all addresses that agreed with claim
+    /// @notice finalize epoch after timeout
+    /// @dev can only be called if challenge period is over
+    function finalizeEpoch() external;
+    
+    /// @notice called when new input arrives, manages the phase changes
+    /// @dev can only be called by input contract
+    function notifyInput() external returns (bool);
 
-        // address claimer and address challenger
-        // will probably live in the dispute resolution contract
-        // they only exist if there is a dispute
-
-        Phase phase; // phase of current state
-    }
-
-
-    // DescartesV2 only keeps the summary of all epochs, using the Merkle Hash
-    bytes32 epochsMRH; // Merkle root hash of epochs
-    /*
-    * Suggested epochsMRH structure
-    *                                    +-----+
-    *                                    | MRH |
-    *                                    +--+--+
-    *                                       |
-    *                           +-------+   |  +-------+
-    *                           |Epoch 1|---+--|Epoch 2|
-    *                           +---+---+      +-------+
-    *                               |
-    *                   +-----+     |    +-----+
-    *                   |I/O 1|-----+----|I/0 2|
-    *                   +--+--+          +-----+
-    *                      |
-    *          +-------+   |   +---------+
-    *          |Input 1|---+---|Output MR|
-    *          +-------+       +---------+
-    *                               |
-    *                  +--------+   |     +--------+
-    *                  |Output 0|---+-----|Output 1|
-    *                  +--------+         +--------+
-    *
-    *
-    * TODO: discuss what here has to be accessed frequently by the chain
-    * to weight the tradeoffs between storage/processing
-    */
-
-    // max number of machine cycles accross every possible dapp computation
-    uint64 maxCycle;
-
-    // contract responsible for input/output management
-    address IOManager;
-
-    // contract that manages the validators
-    address validatorManager;
-
-    // contract that manages this dapps disputes
-    address disputeManager;
-
-    // this might move to IOManager as well
-    // new inputs after inputWindow get accumulated for next epoch
-    uint64 inputWindow;  // time in seconds each epoch waits for inputs
-                         // if there is a challenge/invalid state, the inputs
-                         // get accumulated to the next epoch
-
-    uint64 challengePeriod; // after challengePeriod the current state is finalized
-
-    State state; // current state
-
-// THIS BELONGS TO IOManager
-//    struct Output {
-//        bytes32 outputHash; // hash of output - includes destination, payload, input hash and maybe highestGas
-//
-//        // THIS MOVES TO THE BRIDGE CONTRACT
-//        bool executed; // true if executed without reverting
-//        bytes32[] dependencies; // outputs that this output depends on
-//                                // can only be executed if all of them have been
-//                                // properly executed (executed == true)
-//    }
-
-
-    function claim(bytes32 _epochHash) public {
-        // check if its the first claim
-        // if not, if it agrees with previous claim update the attestorMask
-        // if attestors mask == consensus mask (validatorManager.getConsensusMask?)
-        //      state change from awaiting consensus -> accumulating input
-        // if it doesnt agree instantiates a dispute
-    }
-
-    function finalizeEpoch() public {
-        // anyone can call
-
-        // require phase == WaitingClaims && claim deadline passed
-        // ||
-        // require phase == ChallengeInProgress &&
-        //                  (challenge deadline passed || there is a winner)
-    }
-
-    // descartesv2 gets notified when there is a new input
-    // only by input contract
-    function notifyInput() public returns (bool) {
-
-    }
-    // THIS GOES TO IOContract
-    // function executeOutput(
-    //     Epoch _outputEpoch,
-    //     bytes32 _outputHash,
-    //     uint256 _outputIndex, // index of the output leaf
-    //     bytes32[] _inputSiblings, // Siblings to prove inputs merkle root hash
-    //     bytes32[] _outputSiblings, // Siblings to prove outputs merkle root hash
-    //     bytes32 _payload, // should be contained in outputHash?
-    //     address _destination // should be contained in outputHash?
-    // ) returns (bool) {
-    //     if (executed[_outputHash]) return true;
-    // }
+    /// @notice called when a dispute is resolved by the dispute manager
+    /// @param _winner winner of dispute
+    /// @param _loser lose of sipute
+    /// @param _winningClaim initial claim of winning validator
+    /// @dev can only by the dispute contract 
+    function resolveDispute(
+        address payable _winner,
+        address payable _loser,
+        bytes32 _winningClaim
+    ) external;
 }
