612ce7aa22600727e81582ab33d823c676a9c318
Zehui <zehuizheng@uvic.ca>
2021-05-08 13:16:36 -0700

    test event in constructor; add a flag for modifiers

--- test/DescartesV2Impl.ts
+++ test/DescartesV2Impl.ts
@@ -14,6 +14,10 @@ import { ProjectPathsUserConfig } from 'hardhat/types';
 use(solidity)
 
 describe("Descartes V2 Implementation", ()=> {
+  /// for testing DescartesV2 when modifiers are on, set this to true
+  /// for testing DescartesV2 when modifiers are off, set this to false
+  let permissionModifiersOn = true;
+
   ///mock a contract as if it is already implemented. Check Waffle for details
   let mockInput: MockContract;
   let mockOutput: MockContract;
@@ -267,156 +271,192 @@ describe("Descartes V2 Implementation", ()=> {
     ).to.equal(Phase.InputAccumulation);
   });
 
-/*this section is for tests with modifiers on
-  /// ***test function notifyInput() with modifier*** ///
-  it("only input contract can call notifyInput()", async ()=> {
-    await expect(
-      descartesV2Impl.notifyInput(),
-      'msg.sender != input contract'
-    ).to.be.revertedWith('msg.sender != input contract');
-  });
+  /// modifiers on
+  if (permissionModifiersOn){
+    /// ***test function notifyInput() with modifier*** ///
+    it("only input contract can call notifyInput()", async ()=> {
+      await expect(
+        descartesV2Impl.notifyInput(),
+        'msg.sender != input contract'
+      ).to.be.revertedWith('msg.sender != input contract');
+    });
 
-  /// ***test function resolveDispute() with modifier*** ///
-  it("only DisputeManager contract can call resolveDispute()", async ()=> {
-    await expect(
-      descartesV2Impl.resolveDispute(await signers[0].getAddress(), await signers[1].getAddress(), ethers.utils.formatBytes32String("hello")),
-      'msg.sender != dispute manager contract'
-    ).to.be.revertedWith('msg.sender != dispute manager contract');
-  });
-*/
+    /// ***test function resolveDispute() with modifier*** ///
+    it("only DisputeManager contract can call resolveDispute()", async ()=> {
+      await expect(
+        descartesV2Impl.resolveDispute(await signers[0].getAddress(), await signers[1].getAddress(), ethers.utils.formatBytes32String("hello")),
+        'msg.sender != dispute manager contract'
+      ).to.be.revertedWith('msg.sender != dispute manager contract');
+    });
+  }
 
-///to conduct tests with modifiers on, start commenting out from here
-///Note that there is one more area to comment out at the bottom
-/// /*
-  /// ***test function notifyInput() without modifier*** ///
-  it("notifyInput(): should return false if inputDuration has not past yet", async ()=> {
-    expect(
-      //callStatic is from ethers.js, used to call a state changing function without acutally changing the states. Use callStatic in order to check the return value.
-      await descartesV2Impl.callStatic.notifyInput(),
-      'inputDuration has not past yet'
-    ).to.equal(false);
-  });
 
-  it("notifyInput(): if inputDuration has past and current phase is InputAccumulation, should return true and update the current phase to AwaitingConsensus", async ()=> {
-    await network.provider.send("evm_increaseTime",[inputDuration+1]);
-    await network.provider.send("evm_mine");
+  /// modifiers off
+  if (!permissionModifiersOn){
+    /// ***test function notifyInput() without modifier*** ///
+    it("notifyInput(): should return false if inputDuration has not past yet", async ()=> {
+      expect(
+        //callStatic is from ethers.js, used to call a state changing function without acutally changing the states. Use callStatic in order to check the return value.
+        await descartesV2Impl.callStatic.notifyInput(),
+        'inputDuration has not past yet'
+      ).to.equal(false);
+    });
 
-    expect(
-      await descartesV2Impl.callStatic.notifyInput(),
-      'should return true'
-    ).to.equal(true);
+    it("notifyInput(): if inputDuration has past and current phase is InputAccumulation, should return true and update the current phase to AwaitingConsensus", async ()=> {
+      await network.provider.send("evm_increaseTime",[inputDuration+1]);
+      await network.provider.send("evm_mine");
 
-    await descartesV2Impl.notifyInput();//actually change states before getting current phase
-    expect(
-      await descartesV2Impl.getCurrentPhase(),
-      'the updated current phase'
-    ).to.equal(Phase.AwaitingConsensus);
-  });
+      expect(
+        await descartesV2Impl.callStatic.notifyInput(),
+        'should return true'
+      ).to.equal(true);
 
-  it("notifyInput(): should return false if currentPhase is AwaitingDispute", async ()=> {
-    ///make two different claims///
-    await network.provider.send("evm_increaseTime",[inputDuration+1]);
-    await network.provider.send("evm_mine");
+      await descartesV2Impl.notifyInput();//actually change states before getting current phase
+      expect(
+        await descartesV2Impl.getCurrentPhase(),
+        'the updated current phase'
+      ).to.equal(Phase.AwaitingConsensus);
+    });
 
-    await mockValidatorManager.mock.onClaim.returns(Result.NoConflict,
-      [ethers.utils.formatBytes32String("\0"), ethers.utils.formatBytes32String("\0")],
-      [address_zero, address_zero]
-    );
-    await descartesV2Impl.claim(ethers.utils.formatBytes32String("hello"));
+    it("notifyInput(): should return false if currentPhase is AwaitingDispute", async ()=> {
+      ///make two different claims///
+      await network.provider.send("evm_increaseTime",[inputDuration+1]);
+      await network.provider.send("evm_mine");
 
-    await mockValidatorManager.mock.onClaim.returns(Result.Conflict,
-      [ethers.utils.formatBytes32String("hello"), ethers.utils.formatBytes32String("halo")],
-      [await signers[0].getAddress(), await signers[1].getAddress()]
-    );
-    await mockDisputeManager.mock.initiateDispute.returns();
-    await descartesV2Impl.connect(signers[1]).claim(ethers.utils.formatBytes32String("halo"));
-    ///END: make two different claims///
-    
-    expect(
-      await descartesV2Impl.callStatic.notifyInput(),
-      'phase incorrect'
-    ).to.equal(false);
-  });
+      await mockValidatorManager.mock.onClaim.returns(Result.NoConflict,
+        [ethers.utils.formatBytes32String("\0"), ethers.utils.formatBytes32String("\0")],
+        [address_zero, address_zero]
+      );
+      await descartesV2Impl.claim(ethers.utils.formatBytes32String("hello"));
 
-  it("notifyInput(): if called more than once in an epoch, return false.", async ()=> {
-    await network.provider.send("evm_increaseTime",[inputDuration+1]);
-    await network.provider.send("evm_mine");
-    
-    await descartesV2Impl.notifyInput();//update the block timestamp before calling callStatic
-    //Now the current phase is AwaitingConsensus
-    expect(
-      await descartesV2Impl.callStatic.notifyInput(),
-      'repeated calling'
-    ).to.equal(false);
-  });
+      await mockValidatorManager.mock.onClaim.returns(Result.Conflict,
+        [ethers.utils.formatBytes32String("hello"), ethers.utils.formatBytes32String("halo")],
+        [await signers[0].getAddress(), await signers[1].getAddress()]
+      );
+      await mockDisputeManager.mock.initiateDispute.returns();
+      await descartesV2Impl.connect(signers[1]).claim(ethers.utils.formatBytes32String("halo"));
+      ///END: make two different claims///
+      
+      expect(
+        await descartesV2Impl.callStatic.notifyInput(),
+        'phase incorrect'
+      ).to.equal(false);
+    });
 
-  /// ***test function resolveDispute() without modifier*** ///
-  it("resolveDispute(): if consensus, updated current phase should be InputAccumulation", async ()=> {
-    await mockValidatorManager.mock.onDisputeEnd.returns(
-      Result.Consensus,
-      [ethers.utils.formatBytes32String("hello"), ethers.utils.formatBytes32String("\0")],
-      [await signers[0].getAddress(), address_zero]
-    );
-    await mockValidatorManager.mock.onNewEpoch.returns(ethers.utils.formatBytes32String("hello"));
-    await mockOutput.mock.onNewEpoch.returns();
-    await mockInput.mock.onNewEpoch.returns();
+    it("notifyInput(): if called more than once in an epoch, return false.", async ()=> {
+      await network.provider.send("evm_increaseTime",[inputDuration+1]);
+      await network.provider.send("evm_mine");
+      
+      await descartesV2Impl.notifyInput();
+      //Now the current phase is AwaitingConsensus
+      expect(
+        await descartesV2Impl.callStatic.notifyInput(),
+        'repeated calling'
+      ).to.equal(false);
+    });
 
-    await descartesV2Impl.resolveDispute(
-      await signers[0].getAddress(),
-      await signers[1].getAddress(),
-      ethers.utils.formatBytes32String("hello")
-    );
-    
-    expect(     
-      await descartesV2Impl.getCurrentPhase(),
-      'updated current phase if consensus'
-    ).to.equal(Phase.InputAccumulation);
-  });
+    /// ***test function resolveDispute() without modifier*** ///
+    it("resolveDispute(): if consensus, updated current phase should be InputAccumulation", async ()=> {
+      await mockValidatorManager.mock.onDisputeEnd.returns(
+        Result.Consensus,
+        [ethers.utils.formatBytes32String("hello"), ethers.utils.formatBytes32String("\0")],
+        [await signers[0].getAddress(), address_zero]
+      );
+      await mockValidatorManager.mock.onNewEpoch.returns(ethers.utils.formatBytes32String("hello"));
+      await mockOutput.mock.onNewEpoch.returns();
+      await mockInput.mock.onNewEpoch.returns();
 
-  it("resolveDispute(): if NoConflict, updated current phase should be AwaitingConsensus", async ()=> {
-    await mockValidatorManager.mock.onDisputeEnd.returns(
-      Result.NoConflict,
-      [ethers.utils.formatBytes32String("\0"), ethers.utils.formatBytes32String("\0")],
-      [address_zero, address_zero]
-    );
+      await descartesV2Impl.resolveDispute(
+        await signers[0].getAddress(),
+        await signers[1].getAddress(),
+        ethers.utils.formatBytes32String("hello")
+      );
+      
+      expect(     
+        await descartesV2Impl.getCurrentPhase(),
+        'updated current phase if consensus'
+      ).to.equal(Phase.InputAccumulation);
+    });
 
-    await descartesV2Impl.resolveDispute(
-      await signers[0].getAddress(),
-      await signers[1].getAddress(),
-      ethers.utils.formatBytes32String("hello")
-    );
-    
-    expect(     
-      await descartesV2Impl.getCurrentPhase(),
-      'updated current phase if consensus'
-    ).to.equal(Phase.AwaitingConsensus);
-  });
+    it("resolveDispute(): if NoConflict, updated current phase should be AwaitingConsensus", async ()=> {
+      await mockValidatorManager.mock.onDisputeEnd.returns(
+        Result.NoConflict,
+        [ethers.utils.formatBytes32String("\0"), ethers.utils.formatBytes32String("\0")],
+        [address_zero, address_zero]
+      );
 
-  it("resolveDispute(): if Conflict, updated current phase should be AwaitingDispute", async ()=> {
-    await mockValidatorManager.mock.onDisputeEnd.returns(
-      Result.Conflict,
-      [ethers.utils.formatBytes32String("hello"), ethers.utils.formatBytes32String("hello")],
-      [await signers[0].getAddress(), await signers[0].getAddress()]
-    );
-    await mockDisputeManager.mock.initiateDispute.returns();
+      await descartesV2Impl.resolveDispute(
+        await signers[0].getAddress(),
+        await signers[1].getAddress(),
+        ethers.utils.formatBytes32String("hello")
+      );
+      
+      expect(     
+        await descartesV2Impl.getCurrentPhase(),
+        'updated current phase if consensus'
+      ).to.equal(Phase.AwaitingConsensus);
+    });
 
-    await descartesV2Impl.resolveDispute(
-      await signers[0].getAddress(),
-      await signers[1].getAddress(),
-      ethers.utils.formatBytes32String("hello")
-    );
-    
-    expect(     
-      await descartesV2Impl.getCurrentPhase(),
-      'updated current phase if Conflict'
-    ).to.equal(Phase.AwaitingDispute);
-    //then start new dispute all over again
-  });
-///to conduct tests with modifiers on, finish commenting out here
-///Note that there is one more area to comment out at the bottom
-/// */
+    it("resolveDispute(): if Conflict, updated current phase should be AwaitingDispute", async ()=> {
+      await mockValidatorManager.mock.onDisputeEnd.returns(
+        Result.Conflict,
+        [ethers.utils.formatBytes32String("hello"), ethers.utils.formatBytes32String("hello")],
+        [await signers[0].getAddress(), await signers[0].getAddress()]
+      );
+      await mockDisputeManager.mock.initiateDispute.returns();
+
+      await descartesV2Impl.resolveDispute(
+        await signers[0].getAddress(),
+        await signers[1].getAddress(),
+        ethers.utils.formatBytes32String("hello")
+      );
+      
+      expect(     
+        await descartesV2Impl.getCurrentPhase(),
+        'updated current phase if Conflict'
+      ).to.equal(Phase.AwaitingDispute);
+      //then start new dispute all over again
+    });
+  }
 
   /// ***test emitting events*** ///
+  it("event DescartesV2Created", async ()=> {
+    // we use ethers.js to query historic events
+    // ref: https://docs.ethers.io/v5/single-page/#/v5/getting-started/-%23-getting-started--history
+    let eventFilter = descartesV2Impl.filters.DescartesV2Created(null,null,null,null,null,null);
+    let event = await descartesV2Impl.queryFilter(eventFilter);
+    let eventArgs = event[0]['args']; // get 'args' from the first DescartesV2Created event 
+
+    expect(     
+      eventArgs['_input'],
+      'input address'
+    ).to.equal(mockInput.address);
+
+    expect(     
+      eventArgs['_output'],
+      'output address'
+    ).to.equal(mockOutput.address);
+
+    expect(     
+      eventArgs['_validatorManager'],
+      'Validator Manager address'
+    ).to.equal(mockValidatorManager.address);
+
+    expect(     
+      eventArgs['_disputeManager'],
+      'Dispute Manager address'
+    ).to.equal(mockDisputeManager.address);
+
+    expect(     
+      eventArgs['_inputDuration'],
+      'Input Duration'
+    ).to.equal(inputDuration);
+
+    expect(     
+      eventArgs['_challengePeriod'],
+      'Challenge Period'
+    ).to.equal(challengePeriod);
+  });
 
   it("event Claim", async ()=> {
     await network.provider.send("evm_increaseTime",[inputDuration+1]);
@@ -483,38 +523,36 @@ describe("Descartes V2 Implementation", ()=> {
       .withArgs(numberOfFinalizedEpochs, ethers.utils.formatBytes32String("hello"));
   });
 
-///to conduct tests with modifiers on, start commenting out from here
-///Note that there is one more area to comment out at the above
-/// /*
-  //event ResolveDispute needs to be tested without modifier: onlyDisputeContract
-  it("event ResolveDispute", async ()=> {
-    await network.provider.send("evm_increaseTime",[inputDuration+1]);
-    await network.provider.send("evm_mine");
-    
-    await mockValidatorManager.mock.onDisputeEnd.returns(
-      Result.Consensus,
-      [ethers.utils.formatBytes32String("hello"), ethers.utils.formatBytes32String("\0")],
-      [await signers[0].getAddress(), address_zero]
-    );
-    await mockValidatorManager.mock.onNewEpoch.returns(ethers.utils.formatBytes32String("hello"));
-    await mockOutput.mock.onNewEpoch.returns();
-    await mockInput.mock.onNewEpoch.returns();
 
-    await expect(descartesV2Impl.resolveDispute(
-      await signers[0].getAddress(),
-      await signers[1].getAddress(),
-      ethers.utils.formatBytes32String("hello")
-      )
-    )
-      .to.emit(descartesV2Impl, 'ResolveDispute')
-      .withArgs(
+  /// modifiers off
+  if (!permissionModifiersOn){
+    //event ResolveDispute needs to be tested without modifier: onlyDisputeContract
+    it("event ResolveDispute", async ()=> {
+      await network.provider.send("evm_increaseTime",[inputDuration+1]);
+      await network.provider.send("evm_mine");
+      
+      await mockValidatorManager.mock.onDisputeEnd.returns(
+        Result.Consensus,
+        [ethers.utils.formatBytes32String("hello"), ethers.utils.formatBytes32String("\0")],
+        [await signers[0].getAddress(), address_zero]
+      );
+      await mockValidatorManager.mock.onNewEpoch.returns(ethers.utils.formatBytes32String("hello"));
+      await mockOutput.mock.onNewEpoch.returns();
+      await mockInput.mock.onNewEpoch.returns();
+
+      await expect(descartesV2Impl.resolveDispute(
         await signers[0].getAddress(),
         await signers[1].getAddress(),
         ethers.utils.formatBytes32String("hello")
-      );
-  });
-///to conduct tests with modifiers on, finish commenting out here
-///Note that there is one more area to comment out at the above
-/// */
+        )
+      )
+        .to.emit(descartesV2Impl, 'ResolveDispute')
+        .withArgs(
+          await signers[0].getAddress(),
+          await signers[1].getAddress(),
+          ethers.utils.formatBytes32String("hello")
+        );
+    });
+  }
 });
 
