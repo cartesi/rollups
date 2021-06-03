f7c086c7a152b84931e20e5735bbe0be23a32ffc
Zehui <zehuizheng@uvic.ca>
2021-06-02 19:07:56 -0700

    almost full coverage tests for PortalImpl

--- test/PotalImpl.ts
+++ test/PotalImpl.ts
@@ -27,7 +27,6 @@ import {
 } from "@ethereum-waffle/mock-contract";
 import { solidity, MockProvider, deployContract } from "ethereum-waffle";
 import { PortalImpl__factory } from "../src/types/factories/PortalImpl__factory";
-import { IERC20__factory } from "../src/types/factories/IERC20__factory";
 import { Signer } from "ethers";
 import { PortalImpl } from "../src/types/PortalImpl";
 import { keccak256 } from "ethers/lib/utils";
@@ -37,13 +36,19 @@ use(solidity);
 
 describe("Portal Implementation", async () => {
     let signer: Signer;
+    let signer2: Signer;
     var portalImpl: PortalImpl;
     let mockInput: MockContract; //mock input
     let mockERC20: MockContract; //mock erc20
     let portalFactory: PortalImpl__factory;
 
+    enum operation {
+        EtherOp,
+        ERC20Op,
+    }
+
     beforeEach(async () => {
-        [signer] = await ethers.getSigners();
+        [signer, signer2] = await ethers.getSigners();
 
         const Input = await deployments.getArtifact("InputImpl");
         const CTSI = await deployments.getArtifact("IERC20");
@@ -79,7 +84,7 @@ describe("Portal Implementation", async () => {
                 "0x00",
                 { value: ethers.utils.parseEther("50") }
             ),
-            "ether deposit should revert if amount.length > addresses.length"
+            "ether deposit should revert if amount.length < addresses.length"
         ).to.be.revertedWith("receivers array length != amounts array length");
     });
 
@@ -124,7 +129,9 @@ describe("Portal Implementation", async () => {
                 }
             ),
             "expect etherDeposit function to emit EtherDeposited event"
-        ).to.emit(portalImpl, "EtherDeposited");
+        )
+            .to.emit(portalImpl, "EtherDeposited")
+            .withArgs([await signer.getAddress()], [50], "0x00");
 
         expect(
             await portalImpl.etherDeposit(
@@ -134,7 +141,13 @@ describe("Portal Implementation", async () => {
                 { value: ethers.utils.parseEther("60") }
             ),
             "expect etherDeposit function to emit EtherDeposited event"
-        ).to.emit(portalImpl, "EtherDeposited");
+        )
+            .to.emit(portalImpl, "EtherDeposited")
+            .withArgs(
+                [await signer.getAddress(), mockInput.address],
+                [15, 45],
+                "0x00"
+            );
 
         expect(
             await portalImpl.etherDeposit(
@@ -148,7 +161,34 @@ describe("Portal Implementation", async () => {
                 { value: ethers.utils.parseEther("90") }
             ),
             "expect etherDeposit function to emit EtherDeposited event"
-        ).to.emit(portalImpl, "EtherDeposited");
+        )
+            .to.emit(portalImpl, "EtherDeposited")
+            .withArgs(
+                [
+                    await signer.getAddress(),
+                    mockInput.address,
+                    mockInput.address,
+                ],
+                [15, 45, 30],
+                "0x00"
+            );
+    });
+
+    it("etherDeposit should return the return value of inputContract.addInput()", async () => {
+        let B32str = ethers.utils.formatBytes32String("hello");
+        await mockInput.mock.addInput.returns(B32str);
+
+        expect(
+            await portalImpl.callStatic.etherDeposit(
+                [await signer.getAddress()],
+                [50],
+                "0x00",
+                {
+                    value: ethers.utils.parseEther("50"),
+                }
+            ),
+            "callStatic to check return value"
+        ).to.equal(B32str);
     });
 
     it("erc20Deposit should revert if parameters are inconsistent", async () => {
@@ -171,7 +211,7 @@ describe("Portal Implementation", async () => {
                 [],
                 "0x00"
             ),
-            "ether deposit should revert if amount.length > addresses.length"
+            "ether deposit should revert if amount.length < addresses.length"
         ).to.be.revertedWith("receivers array length != amounts array length");
     });
 
@@ -186,13 +226,14 @@ describe("Portal Implementation", async () => {
                 [50],
                 "0x00"
             ),
-            "ether deposit should revert if amount.length > addresses.length"
+            "ether deposit should revert if erc20 transferFrom fails"
         ).to.be.revertedWith("erc20 transferFrom failed");
     });
 
     it("erc20Deposit should emit events", async () => {
         await mockERC20.mock.transferFrom.returns(true);
-        await mockInput.mock.addInput.returns(keccak256("0x00"));
+        let B32str = keccak256("0x00");
+        await mockInput.mock.addInput.returns(B32str);
 
         expect(
             await portalImpl.erc20Deposit(
@@ -203,7 +244,15 @@ describe("Portal Implementation", async () => {
                 "0x00"
             ),
             "expect etherDeposit function to emit EtherDeposited event"
-        ).to.emit(portalImpl, "ERC20Deposited");
+        )
+            .to.emit(portalImpl, "ERC20Deposited")
+            .withArgs(
+                mockERC20.address,
+                await signer.getAddress(),
+                [await signer.getAddress(), mockInput.address],
+                [15, 45],
+                "0x00"
+            );
 
         expect(
             await portalImpl.erc20Deposit(
@@ -218,6 +267,105 @@ describe("Portal Implementation", async () => {
                 "0x00"
             ),
             "expect etherDeposit function to emit EtherDeposited event"
-        ).to.emit(portalImpl, "ERC20Deposited");
+        )
+            .to.emit(portalImpl, "ERC20Deposited")
+            .withArgs(
+                mockERC20.address,
+                await signer.getAddress(),
+                [
+                    await signer.getAddress(),
+                    mockInput.address,
+                    mockInput.address,
+                ],
+                [1000000, 15, 45],
+                "0x00"
+            );
+    });
+
+    it("erc20Deposit should return the return value of inputContract.addInput()", async () => {
+        await mockERC20.mock.transferFrom.returns(true);
+        let B32str = keccak256("0x00");
+        await mockInput.mock.addInput.returns(B32str);
+
+        expect(
+            await portalImpl.callStatic.erc20Deposit(
+                mockERC20.address,
+                await signer.getAddress(),
+                [await signer.getAddress(), mockInput.address],
+                [15, 45],
+                "0x00"
+            ),
+            "callStatic to check return value"
+        ).to.equal(B32str);
+    });
+
+    it("executeDescartesV2Output should revert if not called from output", async () => {
+        let data = ethers.utils.defaultAbiCoder.encode(
+            ["uint", "uint", "uint", "uint"],
+            [
+                operation.EtherOp,
+                mockERC20.address,
+                await signer.getAddress(),
+                10,
+            ]
+        );
+        await expect(
+            portalImpl.connect(signer2).executeDescartesV2Output(data)
+        ).to.be.revertedWith("msg.sender != outputContract");
+    });
+
+    it("etherWithdrawal should emit EtherWithdrawn and return true if operation is EtherOp", async () => {
+        // deposit ethers to portalImpl for enough balance to call function transfer() in etherWithdrawal()
+        await mockInput.mock.addInput.returns(keccak256("0x00"));
+        await portalImpl.etherDeposit(
+            [await signer.getAddress()],
+            [10],
+            "0x00",
+            {
+                value: ethers.utils.parseEther("10"),
+            }
+        );
+
+        let data = ethers.utils.defaultAbiCoder.encode(
+            ["uint", "uint", "uint", "uint"],
+            [
+                operation.EtherOp,
+                mockERC20.address,
+                await signer.getAddress(),
+                10,
+            ]
+        );
+
+        // callStatic check return value
+        expect(
+            await portalImpl.callStatic.executeDescartesV2Output(data)
+        ).to.equal(true);
+        // check emitted event
+        await expect(portalImpl.executeDescartesV2Output(data))
+            .to.emit(portalImpl, "EtherWithdrawn")
+            .withArgs(await signer.getAddress(), 10);
+    });
+
+    it("etherWithdrawal should emit ERC20Withdrawn and return true if operation is ERC20Op", async () => {
+        await mockERC20.mock.transfer.returns(true);
+
+        let data = ethers.utils.defaultAbiCoder.encode(
+            ["uint", "uint", "uint", "uint"],
+            [
+                operation.ERC20Op,
+                mockERC20.address,
+                await signer.getAddress(),
+                10,
+            ]
+        );
+
+        // callStatic check return value
+        expect(
+            await portalImpl.callStatic.executeDescartesV2Output(data)
+        ).to.equal(true);
+        // check emitted event
+        await expect(portalImpl.executeDescartesV2Output(data))
+            .to.emit(portalImpl, "ERC20Withdrawn")
+            .withArgs(mockERC20.address, await signer.getAddress(), 10);
     });
 });
