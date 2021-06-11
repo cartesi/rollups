f4fe731fa4eaef9eba89f3d1e93146faacb64050
felipeargento <felipe@cartesi.io>
2021-06-11 19:20:12 +0800

    Document finalized_epoch_delegate

--- offchain/src/fold/finalized_epoch_delegate.rs
+++ offchain/src/fold/finalized_epoch_delegate.rs
@@ -53,13 +53,10 @@ impl<DA: DelegateAccess + Send + Sync + 'static> StateFoldDelegate
         let initial_epoch = *initial_state;
 
         let contract = access
-            .build_sync_contract(
-                self.descartesv2_address,
-                block.number,
-                DescartesV2Impl::new,
-            )
+            .build_sync_contract(self.descartesv2_address, block.number, DescartesV2Impl::new)
             .await;
 
+        // Retrieve FinalizeEpoch events
         let epoch_finalized_events = contract
             .finalize_epoch_filter()
             .query_with_meta()
@@ -70,14 +67,17 @@ impl<DA: DelegateAccess + Send + Sync + 'static> StateFoldDelegate
 
         let mut finalized_epochs = FinalizedEpochs::new(initial_epoch);
 
+        // If number of epoch finalized events is smaller than the specified
+        // `inital_epoch` then no update is needed
         if epoch_finalized_events.len() < initial_epoch.as_usize() {
             return Ok(finalized_epochs);
         }
 
         let slice = &epoch_finalized_events[initial_epoch.as_usize()..];
+        // For every event in `epoch_finalized_events`, considering the
+        // `initial_epoch` slice, add a `FinalizedEpoch` to the list
         for (ev, meta) in slice {
-            let inputs =
-                self.get_inputs_sync(ev.epoch_number, block.hash).await?;
+            let inputs = self.get_inputs_sync(ev.epoch_number, block.hash).await?;
 
             let epoch = FinalizedEpoch {
                 epoch_number: ev.epoch_number,
@@ -103,27 +103,18 @@ impl<DA: DelegateAccess + Send + Sync + 'static> StateFoldDelegate
         // Check if there was (possibly) some log emited on this block.
         // As finalized epochs' inputs will not change, we can return early
         // without querying the input StateFold.
-        if !(fold_utils::contains_address(
-            &block.logs_bloom,
-            &self.descartesv2_address,
-        ) && fold_utils::contains_topic(
-            &block.logs_bloom,
-            &previous_state.next_epoch(),
-        ) && fold_utils::contains_topic(
-            &block.logs_bloom,
-            &FinalizeEpochFilter::signature(),
-        )) {
+        if !(fold_utils::contains_address(&block.logs_bloom, &self.descartesv2_address)
+            && fold_utils::contains_topic(&block.logs_bloom, &previous_state.next_epoch())
+            && fold_utils::contains_topic(&block.logs_bloom, &FinalizeEpochFilter::signature()))
+        {
             return Ok(previous_state.clone());
         }
 
         let contract = access
-            .build_fold_contract(
-                self.descartesv2_address,
-                block.hash,
-                DescartesV2Impl::new,
-            )
+            .build_fold_contract(self.descartesv2_address, block.hash, DescartesV2Impl::new)
             .await;
 
+        // Retrieve finalized epoch events
         let epoch_finalized_events = contract
             .finalize_epoch_filter()
             .query_with_meta()
@@ -132,15 +123,17 @@ impl<DA: DelegateAccess + Send + Sync + 'static> StateFoldDelegate
                 err: "Error querying for descartes finalized epochs",
             })?;
 
+        // Clone previous finalized epochs to the current list
         let mut finalized_epochs = previous_state.clone();
 
+        // For every event create a new `FinalizedEpoch` and add it
+        // to the list
         for (ev, meta) in epoch_finalized_events {
             if ev.epoch_number < finalized_epochs.next_epoch() {
                 continue;
             }
 
-            let inputs =
-                self.get_inputs_fold(ev.epoch_number, block.hash).await?;
+            let inputs = self.get_inputs_fold(ev.epoch_number, block.hash).await?;
 
             let epoch = FinalizedEpoch {
                 epoch_number: ev.epoch_number,
@@ -157,17 +150,12 @@ impl<DA: DelegateAccess + Send + Sync + 'static> StateFoldDelegate
         Ok(finalized_epochs)
     }
 
-    fn convert(
-        &self,
-        accumulator: &BlockState<Self::Accumulator>,
-    ) -> Self::State {
+    fn convert(&self, accumulator: &BlockState<Self::Accumulator>) -> Self::State {
         accumulator.clone()
     }
 }
 
-impl<DA: DelegateAccess + Send + Sync + 'static>
-    FinalizedEpochFoldDelegate<DA>
-{
+impl<DA: DelegateAccess + Send + Sync + 'static> FinalizedEpochFoldDelegate<DA> {
     async fn get_inputs_sync<A: SyncAccess + Send + Sync + 'static>(
         &self,
         epoch: U256,
