8879dfaeb489acd5c500a66d0d42c875e42920f0
Gabriel Coutinho <gabriel.coutinho@cartesi.io>
2021-06-17 09:27:56 -0300

    Finish logic draft

--- offchain/src/logic/descartes_logic.rs
+++ offchain/src/logic/descartes_logic.rs
@@ -13,6 +13,7 @@ use dispatcher::state_fold::types::BlockState;
 
 use async_recursion::async_recursion;
 use ethers::core::types::{Address, U256, U64};
+use im::Vector;
 use snafu::ResultExt;
 
 pub struct Config {
@@ -36,7 +37,7 @@ pub struct Config {
     initial_epoch: U256,
 }
 
-async fn main_loop(config: &Config) -> Result<()> {
+async fn main_loop(config: &Config, sender: Address) -> Result<()> {
     let (subscriber_handle, block_subscriber, tx_manager) =
         instantiate_tx_manager(&config.into()).await?;
     let state_fold = instantiate_state_fold(&config.into())?;
@@ -56,7 +57,7 @@ async fn main_loop(config: &Config) -> Result<()> {
                     .await
                     .context(StateFoldError {})?;
 
-                react(state, &tx_manager, &MockMachine {}).await;
+                react(&sender, state, &tx_manager, &MockMachine {}).await;
             }
 
             Err(e) => return Err(Error::SubscriberReceiveError { source: e }),
@@ -67,19 +68,19 @@ async fn main_loop(config: &Config) -> Result<()> {
 }
 
 async fn react<M: MachineInterface + Sync>(
+    sender: &Address,
     state: BlockState<DescartesV2State>,
     tx_manager: &DescartesTxManager,
     machine_manager: &M,
 ) -> Result<()> {
     let state = state.state;
 
-    let current_epoch_status =
-        machine_manager.get_current_epoch_status().await?;
+    let mm_epoch_status = machine_manager.get_current_epoch_status().await?;
 
-    let (should_continue, current_epoch_status) =
+    let (should_continue, mm_epoch_status) =
         enqueue_inputs_of_finalized_epochs(
             &state,
-            current_epoch_status,
+            mm_epoch_status,
             machine_manager,
         )
         .await?;
@@ -92,7 +93,15 @@ async fn react<M: MachineInterface + Sync>(
         PhaseState::InputAccumulation {} => {
             // Discover latest MM accumulating input index
             // Enqueue diff one by one
+            enqueue_remaning_inputs(
+                &mm_epoch_status,
+                &state.current_epoch.inputs.inputs,
+                machine_manager,
+            )
+            .await?;
+
             // React idle.
+            return Ok(());
         }
 
         PhaseState::EpochSealedAwaitingFirstClaim { sealed_epoch } => {
@@ -108,30 +117,77 @@ async fn react<M: MachineInterface + Sync>(
             // If MM is on accumulating epoch, get claim of previous
             // epoch (sealed) and
             // React claim
+            let sealed_epoch_number = state.finalized_epochs.next_epoch();
+            if mm_epoch_status.epoch_number == sealed_epoch_number {
+                let all_inputs_processed = update_sealed_epoch(
+                    sealed_epoch_number,
+                    &sealed_epoch.inputs.inputs,
+                    &mm_epoch_status,
+                    machine_manager,
+                )
+                .await?;
+
+                if !all_inputs_processed {
+                    // React Idle
+                    return Ok(());
+                }
+            }
+
+            // Enqueue accumulating epoch.
+            enqueue_remaning_inputs(
+                &mm_epoch_status,
+                &state.current_epoch.inputs.inputs,
+                machine_manager,
+            )
+            .await?;
+
+            let claim =
+                machine_manager.get_epoch_claim(sealed_epoch_number).await?;
+
+            // TODO: Send claim
         }
 
         // F: I actually have the feeling that AwaitingConsensusNoConflict
         //  and AwaitingConsensusAfterConflict should be unified. The decision
         //  making for them is the same, they only differ for the delegate
         //  to check if the Consensus has timedout or not.
-        PhaseState::AwaitingConsensusNoConflict { claimed_epoch } => {
-            // On AwaitingConsensusNoConflict we have two unfinalized epochs:
-            // sealed and accumulating.
-            //
-            // If MM is on sealed epoch, discover latest MM input index.
-            // enqueue remaining inputs and SessionFinishEpochRequest.
-            //
-            // Check if validator's address has claimed, if not call
-            // SessionFinishEpochRequest and
-            // React claim.
-            //
-            // Then, enqueue accumulating inputs.
-        }
-
-        PhaseState::AwaitingConsensusAfterConflict {
-            claimed_epoch,
-            challenge_period_base_ts,
+        PhaseState::AwaitingConsensusNoConflict { claimed_epoch }
+        | PhaseState::AwaitingConsensusAfterConflict {
+            claimed_epoch, ..
         } => {
+            let sealed_epoch_number = state.finalized_epochs.next_epoch();
+            if mm_epoch_status.epoch_number == sealed_epoch_number {
+                let all_inputs_processed = update_sealed_epoch(
+                    sealed_epoch_number,
+                    &claimed_epoch.inputs.inputs,
+                    &mm_epoch_status,
+                    machine_manager,
+                )
+                .await?;
+
+                if !all_inputs_processed {
+                    // React Idle
+                    return Ok(());
+                }
+            }
+
+            // Enqueue accumulating epoch.
+            enqueue_remaning_inputs(
+                &mm_epoch_status,
+                &state.current_epoch.inputs.inputs,
+                machine_manager,
+            )
+            .await?;
+
+            let sender_claim = claimed_epoch.claims.get_sender_claim(sender);
+            if sender_claim.is_none() {
+                let claim = machine_manager
+                    .get_epoch_claim(sealed_epoch_number)
+                    .await?;
+
+                // TODO:  Send claim
+            }
+
             // On AwaitingConsensusConflict we have two unfinalized epochs:
             // claimed and accumulating.
             //
@@ -146,6 +202,40 @@ async fn react<M: MachineInterface + Sync>(
         }
 
         PhaseState::ConsensusTimeout { claimed_epoch } => {
+            let sealed_epoch_number = state.finalized_epochs.next_epoch();
+            if mm_epoch_status.epoch_number == sealed_epoch_number {
+                let all_inputs_processed = update_sealed_epoch(
+                    sealed_epoch_number,
+                    &claimed_epoch.inputs.inputs,
+                    &mm_epoch_status,
+                    machine_manager,
+                )
+                .await?;
+
+                if !all_inputs_processed {
+                    // React Idle
+                    return Ok(());
+                }
+            }
+
+            // Enqueue accumulating epoch.
+            enqueue_remaning_inputs(
+                &mm_epoch_status,
+                &state.current_epoch.inputs.inputs,
+                machine_manager,
+            )
+            .await?;
+
+            let sender_claim = claimed_epoch.claims.get_sender_claim(sender);
+            if sender_claim.is_none() {
+                let claim = machine_manager
+                    .get_epoch_claim(sealed_epoch_number)
+                    .await?;
+
+                // TODO:  Send claim
+            } else {
+                // TODO: Call blockchain finalize epoch
+            }
             // On ConsensusTimeout we have two unfinalized epochs:
             // claimed and accumulating.
             //
@@ -167,65 +257,106 @@ async fn react<M: MachineInterface + Sync>(
 }
 
 /// Returns true if react can continue, false otherwise, as well as the new
-/// `current_epoch_status`.
+/// `mm_epoch_status`.
 #[async_recursion]
 async fn enqueue_inputs_of_finalized_epochs<M: MachineInterface + Sync>(
     state: &DescartesV2State,
-    current_epoch_status: EpochStatus,
+    mm_epoch_status: EpochStatus,
     machine_manager: &M,
 ) -> Result<(bool, EpochStatus)> {
     // Checking if there are finalized_epochs beyond the machine manager.
     // TODO: comment on index compare.
-    if current_epoch_status.epoch_number >= state.finalized_epochs.next_epoch()
-    {
-        return Ok((true, current_epoch_status));
+    if mm_epoch_status.epoch_number >= state.finalized_epochs.next_epoch() {
+        return Ok((true, mm_epoch_status));
     }
 
     let inputs = state
         .finalized_epochs
-        .get_epoch(current_epoch_status.epoch_number.as_usize())
+        .get_epoch(mm_epoch_status.epoch_number.as_usize())
         .expect("We should have more `finalized_epochs` than machine manager")
         .inputs;
 
-    if current_epoch_status.processed_input_count == inputs.inputs.len() {
+    if mm_epoch_status.processed_input_count == inputs.inputs.len() {
         assert_eq!(
-            current_epoch_status.pending_input_count, 0,
+            mm_epoch_status.pending_input_count, 0,
             "Pending input count should be zero"
         );
 
-        // Call finalize
+        // Call finish
         machine_manager
             .finish_epoch(
-                current_epoch_status.epoch_number,
-                current_epoch_status.processed_input_count.into(),
+                mm_epoch_status.epoch_number,
+                mm_epoch_status.processed_input_count.into(),
             )
             .await?;
 
-        let current_epoch_status =
+        let mm_epoch_status =
             machine_manager.get_current_epoch_status().await?;
 
         // recursively call enqueue_inputs_of_finalized_epochs
         return enqueue_inputs_of_finalized_epochs(
             &state,
-            current_epoch_status,
+            mm_epoch_status,
             machine_manager,
         )
         .await;
     }
 
-    let inputs_sent_count = current_epoch_status.processed_input_count
-        + current_epoch_status.pending_input_count;
+    enqueue_remaning_inputs(&mm_epoch_status, &inputs.inputs, machine_manager)
+        .await?;
 
-    let input_slice = inputs.inputs.clone().slice(inputs_sent_count..);
+    Ok((false, mm_epoch_status))
+}
+
+async fn enqueue_remaning_inputs<M: MachineInterface>(
+    mm_epoch_status: &EpochStatus,
+    inputs: &Vector<Input>,
+    machine_manager: &M,
+) -> Result<bool> {
+    if mm_epoch_status.processed_input_count == inputs.len() {
+        return Ok(true);
+    }
+
+    let inputs_sent_count = mm_epoch_status.processed_input_count
+        + mm_epoch_status.pending_input_count;
+
+    let input_slice = inputs.clone().slice(inputs_sent_count..);
     machine_manager
         .enqueue_inputs(
-            current_epoch_status.epoch_number,
+            mm_epoch_status.epoch_number,
             inputs_sent_count.into(),
             input_slice,
         )
         .await?;
 
-    Ok((false, current_epoch_status))
+    Ok(false)
+}
+
+async fn update_sealed_epoch<M: MachineInterface>(
+    sealed_epoch_number: U256,
+    sealed_inputs: &Vector<Input>,
+    mm_epoch_status: &EpochStatus,
+    machine_manager: &M,
+) -> Result<bool> {
+    let all_inputs_processed = enqueue_remaning_inputs(
+        &mm_epoch_status,
+        &sealed_inputs,
+        machine_manager,
+    )
+    .await?;
+
+    if !all_inputs_processed {
+        return Ok(false);
+    }
+
+    machine_manager
+        .finish_epoch(
+            mm_epoch_status.epoch_number,
+            mm_epoch_status.processed_input_count.into(),
+        )
+        .await?;
+
+    Ok(true)
 }
 
 impl From<&Config> for instantiate_state_fold::Config {
