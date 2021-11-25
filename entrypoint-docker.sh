22d8033ded554ad1ae4c1dfafe43f963ab43de02
Felipe Argento <felipe.argento@cartesi.io>
2021-11-25 10:58:05 -0300

    entrypoint for hardhat docker

---
+++ entrypoint-docker.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# Change to the correct directory
+cd /usr/src/app;
+
+# Run hardhat
+yarn deploy;
+
+# Keep node alive
+set -e
+if [ "${1#-}" != "${1}" ] || [ -z "$(command -v "${1}")" ]; then
+  set -- node "$@"
+fi
+exec "$@"
