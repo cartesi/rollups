e62cc96d0731f176f2da02e65ac12843889286c1
Felipe Argento <felipe.argento@cartesi.io>
2021-11-26 15:50:15 -0300

    Add sleep to delegate/dispatcher entrypoints

--- offchain/delegate_server/entrypoint-delegate-server.sh
+++ offchain/delegate_server/entrypoint-delegate-server.sh
@@ -3,5 +3,7 @@
 # config files
 SF_CONFIG_PATH="/opt/cartesi/share/config/sf-config.toml"
 
+sleep 40
+
 /usr/local/bin/voucher_server_main --sf-config $SF_CONFIG_PATH rollups
 #ENTRYPOINT ["/usr/local/bin/voucher_server_main"]
--- offchain/offchain/entrypoint-dispatcher.sh
+++ offchain/offchain/entrypoint-dispatcher.sh
@@ -9,6 +9,12 @@ SF_CONFIG_PATH="/opt/cartesi/share/config/sf-config.toml"
 BS_CONFIG_PATH="/opt/cartesi/share/config/bs-config.toml"
 TM_CONFIG_PATH="/opt/cartesi/share/config/tm-config.toml"
 
+#while ! test -f "/tmp/$1"; do
+#  sleep 10
+#  echo "Still waiting"
+#done
+sleep 40
+
 /usr/local/bin/offchain_main --sender $SENDER_ADDRESS --rollups-contract-address $ROLLUPS_CONTRACT_ADDRESS \
   --logic-config-path $LOGIC_CONFIG_PATH \
   --sf-config $SF_CONFIG_PATH \
