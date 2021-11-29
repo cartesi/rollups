e5c8200a6b6809f7017a75ce6a7f837b15f7a5bc
Felipe Argento <felipe.argento@cartesi.io>
2021-11-29 19:24:48 -0300

    Update voucher -> output

--- offchain/delegate_server/entrypoint-delegate-server.sh
+++ offchain/delegate_server/entrypoint-delegate-server.sh
@@ -5,5 +5,5 @@ SF_CONFIG_PATH="/opt/cartesi/share/config/sf-config.toml"
 
 sleep 40
 
-/usr/local/bin/voucher_server_main --sf-config $SF_CONFIG_PATH rollups
+/usr/local/bin/output_server_main --sf-config $SF_CONFIG_PATH rollups
 #ENTRYPOINT ["/usr/local/bin/voucher_server_main"]
