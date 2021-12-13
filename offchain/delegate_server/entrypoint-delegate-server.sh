ba73f6f3b3a29282aee7a14e5520c5ea2b07bc21
Gabriel Coutinho <gabriel.coutinho@cartesi.io>
2021-12-13 18:54:28 -0300

    Remove hardcoded contract address
    Use `dockerize` utility to wait for other services to become online.

--- offchain/delegate_server/entrypoint-delegate-server.sh
+++ offchain/delegate_server/entrypoint-delegate-server.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
+dockerize -wait file:///opt/cartesi/share/blockchain/localhost.json
+
 # config files
 SF_CONFIG_PATH="/opt/cartesi/share/config/sf-config.toml"
 
-sleep 40
-
 /usr/local/bin/output_server_main --sf-config $SF_CONFIG_PATH rollups
-#ENTRYPOINT ["/usr/local/bin/voucher_server_main"]
--- offchain/offchain/entrypoint-dispatcher.sh
+++ offchain/offchain/entrypoint-dispatcher.sh
@@ -1,18 +1,17 @@
 #!/bin/sh
+
 # addresses
-ROLLUPS_CONTRACT_ADDRESS="0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9"
+dockerize -wait file:///opt/cartesi/share/blockchain/localhost.json
+ROLLUPS_CONTRACT_ADDRESS=$(jq -r ".contracts.RollupsImpl.address" /opt/cartesi/share/blockchain/localhost.json)
+
+# wait for statefold_server
+dockerize -wait tcp://statefold_server:50051 -timeout 30s
 
 # config files
 LOGIC_CONFIG_PATH="/opt/cartesi/share/config/logic-config.toml"
 SF_CONFIG_PATH="/opt/cartesi/share/config/sf-config.toml"
 BS_CONFIG_PATH="/opt/cartesi/share/config/bs-config.toml"
 TM_CONFIG_PATH="/opt/cartesi/share/config/tm-config.toml"
-
-#while ! test -f "/tmp/$1"; do
-#  sleep 10
-#  echo "Still waiting"
-#done
-sleep 40
 
 /usr/local/bin/offchain_main --rollups-contract-address $ROLLUPS_CONTRACT_ADDRESS \
   --logic-config-path $LOGIC_CONFIG_PATH \
