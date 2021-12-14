f3b5a2744d53bf37e3fb7852f787002ff99c48bc
Gabriel Coutinho <gabriel.coutinho@cartesi.io>
2021-12-14 06:47:28 -0300

    Create `addRepeatedInputs` hardhat task

--- src/tasks/input.ts
+++ src/tasks/input.ts
@@ -65,3 +65,58 @@ task("input:addInput", "Send an input to rollups")
             );
         }
     });
+
+task("input:addRepeatedInputs", "Send an input to rollups")
+    .addParam("input", "bytes to processed by the offchain machine")
+    .addParam("count", "number of repetitions")
+    .addOptionalParam(
+        "signer",
+        "account index of the signer adding the input",
+        0,
+        types.int
+    )
+    .setAction(async (args: TaskArguments, hre: HardhatRuntimeEnvironment) => {
+        const { deployments, ethers } = hre;
+        let input = args.input;
+        let repetitions = args.count;
+        let signerIndex = args.signer;
+        const signers = await ethers.getSigners();
+        if (signerIndex < 0 || signerIndex >= signers.length) {
+            console.error(
+                `Invalid signer account index ${signerIndex}: must be between 0 and ${signers.length}`
+            );
+            return;
+        }
+        const signer = signers[signerIndex];
+
+        let rollupsDeployed = await deployments.get("RollupsImpl");
+
+        let rollups = await ethers.getContractAt(
+            rollupsDeployed.abi,
+            rollupsDeployed.address
+        );
+
+        let inputArtifact = await deployments.getArtifact("InputImpl");
+
+        let inputContract = await ethers.getContractAt(
+            inputArtifact.abi,
+            await rollups.getInputAddress()
+        );
+
+        for(let i = 0; i < repetitions; i ++) {
+            const tx = await inputContract.addInput(input);
+            const events = (await tx.wait()).events;
+            const inputAddedEvent = getEvent("InputAdded", inputContract, events);
+            if (!inputAddedEvent) {
+                console.log(
+                    `Failed to add input '${input}' (signer: ${signer.address}, tx: ${tx.hash})\n`
+                );
+            } else {
+                const epochNumber = inputAddedEvent.args._epochNumber.toString();
+                const timestamp = inputAddedEvent.args._timestamp.toString();
+                console.log(
+                    `Added input '${input}' to epoch '${epochNumber}' (timestamp: ${timestamp}, signer: ${signer.address}, tx: ${tx.hash})`
+                );
+            }
+        }
+    });
