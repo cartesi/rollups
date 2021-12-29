ffd87f12ea45849b9c540aa523d7d27a525c291f
Victor Nogueira <victor.nogueira@cartesi.io>
2021-12-29 11:17:21 -0300

    Increase dockerize timeout on deployment file

--- offchain/delegate_server/entrypoint-delegate-server.sh
+++ offchain/delegate_server/entrypoint-delegate-server.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-dockerize -wait file://${DEPLOYMENT_PATH} -timeout 60s
+dockerize -wait file://${DEPLOYMENT_PATH} -timeout 300s
 
 /usr/local/bin/output_server_main --sf-config $STATE_FOLD_CONFIG_PATH rollups
--- offchain/offchain/entrypoint-dispatcher.sh
+++ offchain/offchain/entrypoint-dispatcher.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 # addresses
-dockerize -wait file://${DEPLOYMENT_PATH} -timeout 60s
+dockerize -wait file://${DEPLOYMENT_PATH} -timeout 300s
 ROLLUPS_CONTRACT_ADDRESS=$(jq -r ".contracts.RollupsImpl.address" ${DEPLOYMENT_PATH})
 
 # wait for services
-dockerize -wait tcp://${STATE_SERVER_HOSTNAME}:${STATE_SERVER_PORT} -wait tcp://${SERVER_MANAGER_HOSTNAME}:${SERVER_MANAGER_PORT} -timeout 30s
+dockerize -wait tcp://${STATE_SERVER_HOSTNAME}:${STATE_SERVER_PORT} -wait tcp://${SERVER_MANAGER_HOSTNAME}:${SERVER_MANAGER_PORT} -timeout 300s
 
 /usr/local/bin/offchain_main --rollups-contract-address $ROLLUPS_CONTRACT_ADDRESS \
   --logic-config-path $LOGIC_CONFIG_PATH \
