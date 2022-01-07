7092f9e77d997a5f8cc889cccdd4f38e4b77cc2b
Guilherme Dantas <guilherme.dantas@cartesi.io>
2022-01-07 10:37:41 -0300

    Update Copyright year to 2022

--- test/EtherPortalFacet.ts
+++ test/EtherPortalFacet.ts
@@ -1,4 +1,4 @@
-// Copyright (C) 2020 Cartesi Pte. Ltd.
+// Copyright (C) 2022 Cartesi Pte. Ltd.
 
 // This program is free software: you can redistribute it and/or modify it under
 // the terms of the GNU General Public License as published by the Free Software
