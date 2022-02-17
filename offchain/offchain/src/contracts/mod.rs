463ba2ac56f4d6d6dfc475d1b85b94469674ba1c
Gabriel Coutinho <gabriel.coutinho@cartesi.io>
2022-02-17 19:32:38 -0300

    Ignore files generated by `build.rs` in rustfmt

--- offchain/offchain/src/contracts/mod.rs
+++ offchain/offchain/src/contracts/mod.rs
@@ -1,13 +1,38 @@
+#[rustfmt::skip]
 pub mod diamond_cut_facet;
+
+#[rustfmt::skip]
 pub mod diamond_init;
+
+#[rustfmt::skip]
 pub mod diamond_loupe_facet;
+
+#[rustfmt::skip]
 pub mod erc20_contract;
+
+#[rustfmt::skip]
 pub mod erc20_portal_facet;
+
+#[rustfmt::skip]
 pub mod erc721_portal_facet;
+
+#[rustfmt::skip]
 pub mod ether_portal_facet;
+
+#[rustfmt::skip]
 pub mod fee_manager_facet;
+
+#[rustfmt::skip]
 pub mod input_facet;
+
+#[rustfmt::skip]
 pub mod output_facet;
+
+#[rustfmt::skip]
 pub mod rollups_facet;
+
+#[rustfmt::skip]
 pub mod serc20_portal_facet;
+
+#[rustfmt::skip]
 pub mod validator_manager_facet;
