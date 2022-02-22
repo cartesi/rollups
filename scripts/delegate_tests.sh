ffdb3fdf0096bb1295ed2e9926a8da3da05188c6
Zehui Zheng <zehui.zheng@cartesi.io>
2022-02-21 20:53:23 -0800

    scripts command path update

--- scripts/delegate_tests.sh
+++ scripts/delegate_tests.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
 
-## run this script in the `onchain/rollups/` directory
-## command `../../scripts/delegate_tests.sh`
+pushd onchain/rollups >/dev/null
 
 npx hardhat node --no-deploy >> /dev/null 2>&1 &
 hardhat_pid=$!
 sleep 3
 
+popd >/dev/null
+
 ## testing input delegate
 
-../../scripts/run_input_server.sh --sf-safety-margin 0 >> /dev/null 2>&1 &
+./scripts/run_input_server.sh --sf-safety-margin 0 >> /dev/null 2>&1 &
 delegate_server_pid=$!
 sleep 3
 
+pushd onchain/rollups >/dev/null
+
 DELEGATE_TEST=1 npx hardhat test test/InputFacet.ts --network localhost
+
+popd >/dev/null
 
 # kill input server
 
@@ -22,12 +27,15 @@ pkill -P "$delegate_server_pid"
 
 ## testing output delegate
 
-../../scripts/run_output_server.sh --sf-safety-margin 0 >> /dev/null 2>&1 &
+./scripts/run_output_server.sh --sf-safety-margin 0 >> /dev/null 2>&1 &
 delegate_server_pid=$!
 sleep 3
 
+pushd onchain/rollups >/dev/null
+
 DELEGATE_TEST=1 npx hardhat test test/OutputFacet.ts --network localhost
 
+popd >/dev/null
 
 # kill output server
 
@@ -36,12 +44,15 @@ pkill -P "$delegate_server_pid"
 
 # testing validator manager delegate
 
-../../scripts/run_validator_manager_server.sh --sf-safety-margin 0 >> /dev/null 2>&1 &
+./scripts/run_validator_manager_server.sh --sf-safety-margin 0 >> /dev/null 2>&1 &
 delegate_server_pid=$!
 sleep 3
 
+pushd onchain/rollups >/dev/null
+
 DELEGATE_TEST=1 npx hardhat test test/ValidatorManagerFacet.ts  --network localhost
 
+popd >/dev/null
 
 # kill validator manager server
 
@@ -50,12 +61,15 @@ pkill -P "$delegate_server_pid"
 
 ## testing fee manager delegate
 
-../../scripts/run_fee_manager_server.sh --sf-safety-margin 0 >> /dev/null 2>&1 &
+./scripts/run_fee_manager_server.sh --sf-safety-margin 0 >> /dev/null 2>&1 &
 delegate_server_pid=$!
 sleep 3
 
+pushd onchain/rollups >/dev/null
+
 DELEGATE_TEST=1 npx hardhat test test/FeeManagerFacet.ts --network localhost
 
+popd >/dev/null
 
 # kill fee manager server
 
@@ -64,12 +78,15 @@ pkill -P "$delegate_server_pid"
 
 ## testing rollups delegate
 
-../../scripts/run_rollups_server.sh --sf-safety-margin 0 >> /dev/null 2>&1 &
+./scripts/run_rollups_server.sh --sf-safety-margin 0 >> /dev/null 2>&1 &
 delegate_server_pid=$!
 sleep 3
 
+pushd onchain/rollups >/dev/null
+
 DELEGATE_TEST=1 npx hardhat test test/RollupsFacet.ts --network localhost
 
+popd >/dev/null
 
 # kill rollups server
 
