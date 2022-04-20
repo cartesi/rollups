8b6bd8e359d5045afc9403c2da3d38ede5237712
Marko Atanasievski <marko.atanasievski@cartesi.io>
2022-04-20 09:28:19 +0200

    Remove old reader

--- reader/src/graphql/resolversMap.ts
+++
@@ -1,7 +0,0 @@
-import { IResolvers } from '@graphql-tools/utils';
-import { merge } from 'lodash'
-import { UserResolvers } from './resolvers'
-
-
-const resolverMap: IResolvers = merge(UserResolvers)
-export default resolverMap
