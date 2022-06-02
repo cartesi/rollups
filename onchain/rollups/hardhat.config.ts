d6e3a9e1e91453ea307b6bb91118631cad73c915
Danilo Tuler <danilo.tuler@cartesi.io>
2022-06-01 20:55:07 -0400

    Remove unused @tenderly/hardhat-tenderly

--- onchain/rollups/hardhat.config.ts
+++ onchain/rollups/hardhat.config.ts
@@ -19,7 +19,6 @@ import "@nomiclabs/hardhat-etherscan";
 import "@nomiclabs/hardhat-waffle";
 import "@typechain/hardhat";
 import "hardhat-deploy";
-import "@tenderly/hardhat-tenderly";
 import "hardhat-abi-exporter";
 import "hardhat-gas-reporter";
 
