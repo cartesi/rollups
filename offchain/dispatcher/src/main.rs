40eb2d4a5e61326e3640ab03035b16e233a28b04
Gabriel Coutinho <gabriel.coutinho@cartesi.io>
2022-07-28 20:03:59 -0300

    Initialize tracing

--- offchain/dispatcher/src/main.rs
+++ offchain/dispatcher/src/main.rs
@@ -2,6 +2,8 @@ use anyhow::Result;
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    tracing_subscriber::fmt::init();
+
     let config = dispatcher::config::DispatcherConfig::initialize_from_args()?;
 
     dispatcher::main_loop::run(config).await?;
--- offchain/state-server/src/rollups_state_server.rs
+++ offchain/state-server/src/rollups_state_server.rs
@@ -2,6 +2,8 @@ use anyhow::Result;
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    tracing_subscriber::fmt::init();
+
     let config =
         state_server_lib::config::StateServerConfig::initialize_from_args()?;
 
