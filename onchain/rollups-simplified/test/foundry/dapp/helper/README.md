14dc3789e40ca747a1be6bcc7aa1d055f4b7056d
Guilherme Dantas <guilherme.dantas@cartesi.io>
2022-10-31 14:59:14 -0300

    Add `--setup` option to `update-proofs.sh`

--- onchain/rollups-simplified/test/foundry/dapp/helper/README.md
+++ onchain/rollups-simplified/test/foundry/dapp/helper/README.md
@@ -1,5 +1,7 @@
 # Updating Proofs
 
+When someone tries to execute a voucher or to validate a notice, they need to provide a proof. This proof is checked on-chain by the DApp contract. In order to test the proof verification scheme, we need to generate proofs and check them with Forge. Most of the heavy lifting is done by the [Server Manager Proof Generator](https://github.com/cartesi-corp/machine-emulator/tree/feature/gen-proofs/tools/gen-proofs), but some scripts in this folder help automate the process of updating the proofs.
+
 ## Dependencies
 
 * GNU sed, awk & grep
@@ -8,6 +10,20 @@
 * jq
 
 ## Setup
+
+Once you've installed all dependencies listed above, there is still some setup left to do.
+
+**Warning:** We use `python3` and `pip3` to interact with Python. If you do not wish to dirty your global Python installation, we recommend you to create a local virtual environment (with `venv` or `pyenv`), and activate it beforehand.
+
+### Automatic Setup
+
+You can simply run the following command. Be aware that you might get prompted to insert your GitHub credentials in order to clone the machine emulator repository.
+
+```sh
+./update-proofs.sh --setup
+```
+
+### Manual Setup
 
 1. Clone the `machine-emulator` repository anywhere you want. In this example, we'll clone it in `~/Cartesi/`.
 
@@ -26,6 +42,7 @@ git checkout feature/gen-proofs
 3. Build the `gen-proofs` Docker image
 
 ```sh
+cd tools/gen-proofs
 docker build -t cartesi/server-manager-gen-proofs:devel .
 ```
 
@@ -37,17 +54,20 @@ pip3 install base64-to-hex-converter
 
 ## Procedure
 
-Now, everytime you think the proofs might need to be updated, just run the following command.
-Feel free to change the path to the `machine-emulator` repository in the command if you haven't cloned it in `~/Cartesi/`.
-If `MACHINE_EMULATOR_REPO` isn't set, it will assume the `machine-emulator` repository is sitting right next to the `rollups` repository.
-In other words, it will assume they have the same parent directory.
+Now, everytime you suspect the proofs might need to be updated, you can simply run the following command.
 
 ```sh
-MACHINE_EMULATOR_REPO=~/Cartesi/machine-emulator
 ./update-proofs.sh
 ```
 
-If you're curious as to how the `update-proofs.sh` script works, here's a diagram of the pipeline.
+## Environment variables
+
+By default, the script assumes the `machine-emulator` repository is on the same directory as the `rollups` repository.
+To change this behaviour, you can set the environment variable `MACHINE_EMULATOR_REPO` to the path of the machine emulator repository on your machine.
+
+## Pipeline
+
+If you're curious to know how the `update-proofs.sh` script works, here's a diagram of the pipeline.
 
 ```mermaid
 graph TD
--- onchain/rollups-simplified/test/foundry/dapp/helper/update-proofs.sh
+++ onchain/rollups-simplified/test/foundry/dapp/helper/update-proofs.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 # Go to the helper folder
 cd "${BASH_SOURCE%/*}"
 
-# Get path from machine emulator repository
+# Get path to machine emulator repository
 if [ -n "${MACHINE_EMULATOR_REPO:-}" ]
 then
     machine_emulator_repo="${MACHINE_EMULATOR_REPO}"
@@ -14,38 +14,105 @@ else
     machine_emulator_repo=${rollups_repo}/../machine-emulator
 fi
 
+# Color numbers
+GREEN=32
+MAGENTA=35
+CYAN=36
+
+# Echoes with color
+echo2() {
+    printf "\033[0;$1m"
+    shift
+    echo "$@"
+    printf "\033[0;00m"
+}
+
+# Check for command line arguments
+if [ $# -ge 1 ] && [ $1 == "--setup" ]
+then
+    echo2 $CYAN "Setting up..."
+
+    if [ -d "${machine_emulator_repo}" ]
+    then
+        echo
+        echo2 $GREEN "1. Machine emulator repository found. Cloning skipped."
+    else
+        echo
+        echo2 $GREEN "1. Machine emulator repository not found. Cloning it..."
+        echo
+
+        # Clone the machine-emulator repository
+        git clone https://github.com/cartesi-corp/machine-emulator.git -- "${machine_emulator_repo}"
+    fi
+
+    # Go to machine emulator repository
+    pushd "${machine_emulator_repo}" >/dev/null
+
+    echo
+    echo2 $GREEN "2. Switching machine emulator branch to 'feature/gen-proofs'..."
+    echo
+
+    # Change branch
+    git checkout feature/gen-proofs
+
+    echo
+    echo2 $GREEN "3. Building Docker image..."
+    echo
+
+    # Build Docker image
+    cd tools/gen-proofs
+    docker build -t cartesi/server-manager-gen-proofs:devel .
+
+    echo
+    echo2 $GREEN "4. Installing Python package..."
+    echo
+
+    # Install Python package through pip
+    pip3 install base64-to-hex-converter
+
+    # Return to rollups repository
+    popd >/dev/null
+
+    echo
+    echo2 $CYAN "All set up!"
+    echo
+
+    # Do not update proofs, just set up.
+    exit 0
+fi
+
 # Get absolute path of helper folder
-HELPER_FOLDER=`pwd`
+helper_folder=`pwd`
 
 # Create a temporary file for storing the test output
-LOG_FILE=`mktemp`
+log_file=`mktemp`
 
-echo "Updating proofs..."
+echo2 $CYAN "Updating proofs..."
 
 echo
-echo "1. Running forge tests..."
+echo2 $GREEN "1. Running forge tests..."
 
 # Run the tests and pipe the output to a file
-forge test -vv --match-contract CartesiDAppTest > "${LOG_FILE}" || true
+forge test -vv --match-contract CartesiDAppTest > "${log_file}" || true
 
 # Echo an error message before exiting
 failure() {
   local lineno=$1
   local msg=$2
-  echo "Failed at ${lineno}: ${msg}"
+  echo2 $MAGENTA "Failed at ${lineno}: ${msg}"
 }
 
 # Install a trap to help debugging
 trap 'failure ${LINENO} "${BASH_COMMAND}"' ERR
 
 echo
-echo "2. Processing logs and updating vouchers JSON..."
+echo2 $GREEN "2. Processing logs and updating vouchers JSON..."
 
 # Process the log file with awk and generate a jq filter
-jq_filter=`awk -f jqFilter.awk -- "${LOG_FILE}"`
+jq_filter=`awk -f jqFilter.awk -- "${log_file}"`
 
 # Remove log file
-rm "${LOG_FILE}"
+rm "${log_file}"
 
 # Run the jq filter on vouchers.json
 jq_output=`jq "${jq_filter}" vouchers.json`
@@ -54,46 +121,47 @@ jq_output=`jq "${jq_filter}" vouchers.json`
 echo "${jq_output}" > vouchers.json
 
 echo
-echo "3. Generating script to be run on docker image..."
+echo2 $GREEN "3. Generating script to be run on docker image..."
 echo
 
 # Generate script with vouchers
-npx ts-node genScript.ts | sed 's/^/* /g'
+npx ts-node genScript.ts | sed 's/^/* /'
 
 echo
-echo "4. Running docker image to generate epoch status..."
+echo2 $GREEN "4. Running docker image to generate epoch status..."
+echo
 
 # Go to gen-proofs folder
 pushd "${machine_emulator_repo}/tools/gen-proofs" >/dev/null
 
 # Copy script to gen-proofs folder
-cp "${HELPER_FOLDER}/gen-proofs.sh" gen-proofs.sh
+cp "${helper_folder}/gen-proofs.sh" gen-proofs.sh
 
 # Run docker to generate proofs
 docker run -it --rm \
     --name gen-proofs \
-    -v "${PWD}/gen-proofs.sh:/opt/gen-proofs/gen-proofs.sh" \
-    -v "${PWD}/output:/opt/gen-proofs/output" \
+    -v "`pwd`/gen-proofs.sh:/opt/gen-proofs/gen-proofs.sh" \
+    -v "`pwd`/output:/opt/gen-proofs/output" \
     -w /opt/gen-proofs \
     cartesi/server-manager-gen-proofs:devel \
-    ./gen-proofs.sh >/dev/null
+    ./gen-proofs.sh
 
 echo
-echo "5. Processing epoch status and updating voucher proofs..."
+echo2 $GREEN "5. Processing epoch status and updating voucher proofs..."
 
 # Decode strings in epoch status from Base64 to hexadecimal
 # Format the output with jq so that git diffs are smoother
-python3 -m b64to16 output/epoch-status.json | jq > "${HELPER_FOLDER}/voucherProofs.json"
+python3 -m b64to16 output/epoch-status.json | jq > "${helper_folder}/voucherProofs.json"
 
 # Go back to the helper folder
 popd >/dev/null
 
 echo
-echo "6. Generating Solidity contracts for each proof..."
+echo2 $GREEN "6. Generating Solidity contracts for each proof..."
 echo
 
 # Generate Solidity libraries with proofs
-npx ts-node genProofLibraries.ts | sed 's/^/* /g'
+npx ts-node genProofLibraries.ts | sed 's/^/* /'
 
 echo
-echo "Proofs were updated!"
+echo2 $CYAN "Proofs were updated!"
