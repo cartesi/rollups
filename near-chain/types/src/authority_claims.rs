743f9f8065f080c76a4f33ea2c7f35e4dc1e0f15
Gabriel Coutinho de Paula <gabriel.coutinho@cartesi.io>
2022-11-17 14:59:27 -0300

    Improve naming in foldables

--- near-chain/types/src/authority_claims.rs
+++ near-chain/types/src/authority_claims.rs
@@ -1,8 +1,7 @@
 use crate::FoldableError;
 
 use state_fold::{
-    utils as fold_utils, FoldMiddleware, Foldable, StateFoldEnvironment,
-    SyncMiddleware,
+    utils as fold_utils, FoldMiddleware, Foldable, StateFoldEnvironment, SyncMiddleware,
 };
 use state_fold_types::{
     ethers::{
@@ -26,7 +25,7 @@ pub struct HistoryInitialState {
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct Claim {
-    pub claim: H256,
+    pub epoch_hash: H256,
 
     // Both "closed/inclusive"
     pub start_input_index: usize,
@@ -36,7 +35,7 @@ pub struct Claim {
 impl From<([u8; 32], u128, u128)> for Claim {
     fn from(x: ([u8; 32], u128, u128)) -> Self {
         Self {
-            claim: x.0.into(),
+            epoch_hash: x.0.into(),
             start_input_index: x.1 as usize,
             end_input_index: x.2 as usize,
         }
@@ -44,14 +43,14 @@ impl From<([u8; 32], u128, u128)> for Claim {
 }
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
-pub struct DAppHistory {
+pub struct DAppClaims {
     pub claims: Vector<Arc<Claim>>,
 }
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct History {
-    pub claims_initial_state: Arc<HistoryInitialState>,
-    pub histories: Arc<HashMap<Arc<Address>, Arc<DAppHistory>>>,
+    pub history_initial_state: Arc<HistoryInitialState>,
+    pub dapp_claims: Arc<HashMap<Arc<Address>, Arc<DAppClaims>>>,
 }
 
 #[async_trait]
@@ -66,15 +65,14 @@ impl Foldable for History {
         _env: &StateFoldEnvironment<M, Self::UserData>,
         access: Arc<SyncMiddleware<M>>,
     ) -> Result<Self, Self::Error> {
-        let claims_initial_state = initial_state.clone();
-        let contract_address = claims_initial_state.history_address;
+        let history_initial_state = initial_state.clone();
+        let contract_address = history_initial_state.history_address;
 
-        let histories =
-            fetch_history(access, contract_address, &HashMap::new()).await?;
+        let dapp_claims = fetch_history(access, contract_address, &HashMap::new()).await?;
 
         Ok(Self {
-            claims_initial_state,
-            histories,
+            history_initial_state,
+            dapp_claims,
         })
     }
 
@@ -85,8 +83,7 @@ impl Foldable for History {
         _env: &StateFoldEnvironment<M, Self::UserData>,
         access: Arc<FoldMiddleware<M>>,
     ) -> Result<Self, Self::Error> {
-        let history_address =
-            previous_state.claims_initial_state.history_address;
+        let history_address = previous_state.history_initial_state.history_address;
 
         if !(fold_utils::contains_address(&block.logs_bloom, &history_address)
             && (fold_utils::contains_topic(
@@ -97,13 +94,12 @@ impl Foldable for History {
             return Ok(previous_state.clone());
         }
 
-        let new_histories =
-            fetch_history(access, history_address, &previous_state.histories)
-                .await?;
+        let new_dapp_claims =
+            fetch_history(access, history_address, &previous_state.dapp_claims).await?;
 
         Ok(Self {
-            claims_initial_state: previous_state.claims_initial_state.clone(),
-            histories: new_histories,
+            history_initial_state: previous_state.history_initial_state.clone(),
+            dapp_claims: new_dapp_claims,
         })
     }
 }
@@ -111,12 +107,12 @@ impl Foldable for History {
 async fn fetch_history<M: Middleware + 'static>(
     provider: Arc<M>,
     contract_address: Address,
-    previous_histories: &HashMap<Arc<Address>, Arc<DAppHistory>>,
-) -> Result<Arc<HashMap<Arc<Address>, Arc<DAppHistory>>>, FoldableError> {
+    previous_dapp_claims: &HashMap<Arc<Address>, Arc<DAppClaims>>,
+) -> Result<Arc<HashMap<Arc<Address>, Arc<DAppClaims>>>, FoldableError> {
     use contracts::history::*;
     let contract = History::new(contract_address, Arc::clone(&provider));
 
-    let mut histories = previous_histories.clone();
+    let mut dapp_claims = previous_dapp_claims.clone();
 
     // Retrieve `NewClaim` events
     let claims = contract
@@ -129,7 +125,7 @@ async fn fetch_history<M: Middleware + 'static>(
         let new_claim: Arc<Claim> = Arc::new(claim.claim.into());
         let dapp_address = Arc::new(claim.dapp);
 
-        histories
+        dapp_claims
             .entry(dapp_address)
             .and_modify(|h| {
                 let mut new_history = (**h).clone();
@@ -137,11 +133,11 @@ async fn fetch_history<M: Middleware + 'static>(
                 *h = Arc::new(new_history);
             })
             .or_insert_with(|| {
-                Arc::new(DAppHistory {
+                Arc::new(DAppClaims {
                     claims: im::vector![new_claim],
                 })
             });
     }
 
-    Ok(Arc::new(histories))
+    Ok(Arc::new(dapp_claims))
 }
