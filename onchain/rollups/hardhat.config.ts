51ce4abba883b1bf926a3dbdbf6a5bc5c3bb27c2
Danilo Tuler <danilo.tuler@cartesi.io>
2023-01-10 20:50:24 -0300

    Configure networks

--- onchain/rollups/hardhat.config.ts
+++ onchain/rollups/hardhat.config.ts
@@ -53,10 +53,24 @@ const config: HardhatUserConfig = {
         },
         mainnet: infuraNetwork("mainnet", 1, 6283185),
         goerli: infuraNetwork("goerli", 5, 6283185),
+        sepolia: infuraNetwork("sepolia", 11155111, 6283185),
         polygon_mumbai: infuraNetwork("polygon-mumbai", 80001),
+        arbitrum_goerli: infuraNetwork("arbitrum-goerli", 421613),
+        optimism_goerli: infuraNetwork("optimism-goerli", 420),
         bsc_testnet: {
             url: "https://data-seed-prebsc-1-s1.binance.org:8545",
             chainId: 97,
+            accounts: mnemonic ? { mnemonic } : undefined,
+        },
+        iotex_testnet: {
+            url: "https://babel-api.testnet.iotex.io",
+            chainId: 4690,
+            accounts: mnemonic ? { mnemonic } : undefined,
+        },
+        chiado: {
+            url: "https://rpc.chiadochain.net",
+            chainId: 10200,
+            gasPrice: 1000000000,
             accounts: mnemonic ? { mnemonic } : undefined,
         },
     },
@@ -95,10 +109,21 @@ const config: HardhatUserConfig = {
             localhost: ["deployments/localhost"],
             mainnet: [ppath("@cartesi/util", "/deployments/mainnet")],
             goerli: [ppath("@cartesi/util", "/deployments/goerli")],
+            sepolia: [ppath("@cartesi/util", "/deployments/sepolia")],
             polygon_mumbai: [
                 ppath("@cartesi/util", "/deployments/polygon_mumbai"),
             ],
+            arbitrum_goerli: [
+                ppath("@cartesi/util", "/deployments/arbitrum_goerli"),
+            ],
+            optimism_goerli: [
+                ppath("@cartesi/util", "/deployments/optimism_goerli"),
+            ],
             bsc_testnet: [ppath("@cartesi/util", "/deployments/bsc_testnet")],
+            iotex_testnet: [
+                ppath("@cartesi/util", "/deployments/iotex_testnet"),
+            ],
+            chiado: [ppath("@cartesi/util", "/deployments/chiado")],
         },
     },
     namedAccounts: {
