82611df8bccdddac7786535d3ebe20f2c01dfe06
Guilherme Dantas <guilherme.dantas@cartesi.io>
2023-03-08 12:05:28 +0700

    chore(onchain): ignore proofs
    * Running `./update-proofs.sh --setup` now bootstraps proofs

--- onchain/rollups/test/foundry/dapp/helper/update-proofs.sh
+++ onchain/rollups/test/foundry/dapp/helper/update-proofs.sh
@@ -7,6 +7,12 @@ GREEN=32
 MAGENTA=35
 CYAN=36
 
+# Step counter
+STEP=1
+
+# Number of inputs
+NINPUTS=4
+
 # Echo with color
 echo2() {
     printf "\033[0;$1m"
@@ -15,11 +21,52 @@ echo2() {
     printf "\033[0;00m"
 }
 
+# Echo with color and step counter
+echo3() {
+    color=$1
+    message=$2
+    shift 2
+    echo2 "$color" "$((STEP++)). $message" "$@"
+}
+
 # Echo an error message before exiting
 failure() {
   local lineno=$1
   local msg=$2
   echo2 $MAGENTA "Failed at ${lineno}: ${msg}"
+}
+
+# Generate shell script for off-chain machine
+# based on the inputs JSON file
+genscript() {
+    pushd ../../../../../ >/dev/null
+    npx ts-node rollups/test/foundry/dapp/helper/genScript.ts
+    popd >/dev/null
+}
+
+# Run the Cartesi Machine responsible for generating
+# the epoch status JSON file
+runmachine() {
+    docker run -it --rm \
+        --name gen-proofs \
+        -v "`pwd`/gen-proofs.sh:/opt/gen-proofs/gen-proofs.sh" \
+        -v "`pwd`/output:/opt/gen-proofs/output" \
+        -w /opt/gen-proofs \
+        cartesi/server-manager-gen-proofs:devel \
+        ./gen-proofs.sh
+}
+
+# Decode strings in epoch status from Base64 to hexadecimal
+# Format the output with jq so that git diffs are smoother
+b64to16() {
+    python3 -m b64to16 output/epoch-status.json | jq > epoch-status.json
+}
+
+# Generate Solidity libraries with proofs from epoch status
+genlibs() {
+    pushd ../../../../../ >/dev/null
+    npx ts-node rollups/test/foundry/dapp/helper/genProofLibraries.ts
+    popd >/dev/null
 }
 
 # Install a trap to help debugging
@@ -34,30 +81,68 @@ then
     echo2 $CYAN "Setting up..."
     echo
 
-    echo2 $GREEN "1. Building Docker image..."
+    echo3 $GREEN "Building Docker image..."
     echo
 
     # Build Docker image
     docker build -t cartesi/server-manager-gen-proofs:devel .
     echo
 
-    echo2 $GREEN "2. Installing Python packages..."
+    echo3 $GREEN "Installing Python packages..."
     echo
 
     # Install Python packages with pip3
     pip3 install -r requirements.txt
     echo
-    
-    echo2 $CYAN "All set up!"
 
-    # Do not update proofs, just set up.
-    exit 0
+    echo3 $GREEN "Generating dummy inputs..."
+    echo
+
+    # Generate a dummy inputs JSON
+    inputs="[]"
+    sender="0xdeadbeefdeadbeefdeadbeefdeadbeefdeadbeef"
+    payload="0x"
+    for (( i=0; i<$NINPUTS; i++ ))
+    do
+        inputs=`echo "$inputs" | jq ".[$i].sender = \"$sender\""`
+        inputs=`echo "$inputs" | jq ".[$i].payload = \"$payload\""`
+    done
+    echo "$inputs" > inputs.json
+
+    echo3 $GREEN "Generating script to be run on docker image..."
+    echo
+
+    genscript
+    echo
+
+    # Give execute permission to script
+    chmod +x gen-proofs.sh
+
+    echo3 $GREEN "Running docker image to generate epoch status..."
+    echo
+
+    runmachine
+    echo
+
+    echo3 $GREEN "Processing and formatting epoch status..."
+    echo
+
+    b64to16
+
+    echo3 $GREEN "Generating Solidity libraries for each output..."
+    echo
+
+    genlibs
+    echo
+
+    echo2 $CYAN "All set up!"
+    echo
 fi
 
 echo2 $CYAN "Updating proofs..."
 echo
 
-echo2 $GREEN "0. Running yarn and prettier..."
+echo3 $GREEN "Running yarn and prettier..."
 echo
 
 yarn
@@ -66,13 +151,13 @@ echo
 yarn prettier --write
 echo
 
-echo2 $GREEN "1. Running forge tests..."
+echo3 $GREEN "Running forge tests..."
 echo
 
 # Run the tests with forge and store the output
 forge_output=`forge test -vv --match-contract CartesiDAppTest || true`
 
-echo2 $GREEN "2. Processing forge output and updating inputs JSON..."
+echo3 $GREEN "Processing forge output and updating inputs JSON..."
 echo
 
 # Process the forge output with awk and generate a jq filter
@@ -84,41 +169,27 @@ jq_output=`jq "${jq_filter}" inputs.json`
 # Update inputs.json
 echo "${jq_output}" > inputs.json
 
-echo2 $GREEN "3. Generating script to be run on docker image..."
+echo3 $GREEN "Generating script to be run on docker image..."
 echo
 
-# Generate script with inputs
-pushd ../../../../../ && npx ts-node rollups/test/foundry/dapp/helper/genScript.ts && popd
+genscript
 echo
 
-# Give execute permission to script
-chmod +x gen-proofs.sh
-
-echo2 $GREEN "4. Running docker image to generate epoch status..."
+echo3 $GREEN "Running docker image to generate epoch status..."
 echo
 
-# Run docker to generate proofs
-docker run -it --rm \
-    --name gen-proofs \
-    -v "`pwd`/gen-proofs.sh:/opt/gen-proofs/gen-proofs.sh" \
-    -v "`pwd`/output:/opt/gen-proofs/output" \
-    -w /opt/gen-proofs \
-    cartesi/server-manager-gen-proofs:devel \
-    ./gen-proofs.sh
+runmachine
 echo
 
-echo2 $GREEN "5. Processing and formatting epoch status..."
+echo3 $GREEN "Processing and formatting epoch status..."
 echo
 
-# Decode strings in epoch status from Base64 to hexadecimal
-# Format the output with jq so that git diffs are smoother
-python3 -m b64to16 output/epoch-status.json | jq > epoch-status.json
+b64to16
 
-echo2 $GREEN "6. Generating Solidity libraries for each output..."
+echo3 $GREEN "Generating Solidity libraries for each output..."
 echo
 
-# Generate Solidity libraries with proofs
-pushd ../../../../../ && npx ts-node rollups/test/foundry/dapp/helper/genProofLibraries.ts && popd
+genlibs
 echo
 
 echo2 $CYAN "Proofs were updated!"
