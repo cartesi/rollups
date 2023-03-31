a7ce311242046f3b1545f02bf31b3d2f6a3daba7
Guilherme Dantas <guilherme.dantas@cartesi.io>
2023-03-31 09:15:33 -0300

    chore(onchain): change pragma version of `SimpleConsensus`

--- onchain/rollups/test/foundry/util/SimpleConsensus.sol
+++ onchain/rollups/test/foundry/util/SimpleConsensus.sol
@@ -11,7 +11,7 @@
 // specific language governing permissions and limitations under the License.
 
 /// @title A Simple Consensus Contract
-pragma solidity 0.8.13;
+pragma solidity ^0.8.8;
 
 import {AbstractConsensus} from "contracts/consensus/AbstractConsensus.sol";
 
