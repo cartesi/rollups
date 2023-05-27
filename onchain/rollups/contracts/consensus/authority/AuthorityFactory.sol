926504ba0617dcfabb86061f2e187e1b85b74d60
Pedro Argento <pedro.argento@gmail.com>
2023-05-27 10:57:09 -0300

    feat(onchain-contracts): Add authority factorty

---
+++ onchain/rollups/contracts/consensus/authority/AuthorityFactory.sol
@@ -0,0 +1,88 @@
+// Copyright Cartesi Pte. Ltd.
+
+// SPDX-License-Identifier: Apache-2.0
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+// this file except in compliance with the License. You may obtain a copy of the
+// License at http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software distributed
+// under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+// CONDITIONS OF ANY KIND, either express or implied. See the License for the
+// specific language governing permissions and limitations under the License.
+
+pragma solidity ^0.8.8;
+
+import {IAuthorityFactory} from "./IAuthorityFactory.sol";
+import {Authority} from "./Authority.sol";
+import {IInputBox} from "../../inputs/IInputBox.sol";
+
+/// @title Authority Factory
+/// @notice Allows anyone to reliably deploy a new `Authority` contract.
+contract AuthorityFactory is IAuthorityFactory {
+    function newAuthority(
+        address _authorityOwner,
+        IInputBox _inputBox
+    ) external override returns (Authority) {
+        Authority authority = new Authority(
+            _authorityOwner,
+            _inputBox
+        );
+
+        emit AuthorityCreated(
+            _authorityOwner,
+            _inputBox,
+            authority
+        );
+
+        return authority;
+    }
+
+    function newAuthority(
+        address _authorityOwner,
+        IInputBox _inputBox,
+        bytes32 _salt
+    ) external override returns (Authority) {
+        Authority authority = new Authority{salt: _salt}(
+            _authorityOwner,
+            _inputBox
+        );
+
+        emit AuthorityCreated(
+            _authorityOwner,
+            _inputBox,
+            authority
+        );
+
+        return authority;
+   }
+
+    function calculateAuthorityAddress(
+        address _authorityOwner,
+        IInputBox _inputBox,
+        bytes32 _salt
+    ) external view override returns (address) {
+        return
+            address(
+                uint160(
+                    uint256(
+                        keccak256(
+                            abi.encodePacked(
+                                bytes1(0xff),
+                                address(this),
+                                _salt,
+                                keccak256(
+                                    abi.encodePacked(
+                                        type(Authority).creationCode,
+                                        abi.encode(
+                                            _authorityOwner,
+                                            _inputBox
+                                        )
+                                    )
+                                )
+                            )
+                        )
+                    )
+                )
+            );
+    }
+}
---
+++ onchain/rollups/contracts/consensus/authority/IAuthorityFactory.sol
@@ -0,0 +1,65 @@
+// Copyright Cartesi Pte. Ltd.
+
+// SPDX-License-Identifier: Apache-2.0
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+// this file except in compliance with the License. You may obtain a copy of the
+// License at http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software distributed
+// under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+// CONDITIONS OF ANY KIND, either express or implied. See the License for the
+// specific language governing permissions and limitations under the License.
+
+pragma solidity ^0.8.8;
+
+import {Authority} from "./Authority.sol";
+import {IInputBox} from "../../inputs/IInputBox.sol";
+
+/// @title Cartesi DApp Factory interface
+interface IAuthorityFactory {
+    // Events
+
+    /// @notice A new authority was deployed.
+    /// @param authorityOwner The initial DApp owner
+    /// @dev MUST be triggered on a successful call to `newAuthority`.
+    event AuthorityCreated(
+        address authorityOwner,
+        IInputBox _inputBox,
+        Authority authority
+    );
+
+    // Permissionless functions
+
+    /// @notice Deploy a new authority.
+    /// @param _authorityOwner The initial authority owner
+    /// @return The authority
+    /// @dev On success, MUST emit an `AuthorityCreated` event.
+    function newAuthority(
+        address _authorityOwner,
+        IInputBox _inputBox
+    ) external returns (Authority);
+
+    /// @notice Deploy a new authority deterministically.
+    /// @param _authorityOwner The initial DApp owner
+    /// @param _salt The salt used to deterministically generate the DApp address
+    /// @return The authority
+    /// @dev On success, MUST emit an `AuthorityCreated` event.
+    function newAuthority(
+        address _authorityOwner,
+        IInputBox _inputBox,
+        bytes32 _salt
+    ) external returns (Authority);
+
+    /// @notice Calculate the address of an authority to be deployed deterministically.
+    /// @param _authorityOwner The initial authority owner
+    /// @param _inputBox The input box
+    /// @param _salt The salt used to deterministically generate the DApp address
+    /// @return The deterministic authority address
+    /// @dev Beware that only the `newAuthority` function with the `_salt` parameter
+    ///      is able to deterministically deploy an authority.
+    function calculateAuthorityAddress(
+        address _authorityOwner,
+        IInputBox _inputBox,
+        bytes32 _salt
+    ) external view returns (address);
+}
