3bf401163105c381a1ac8af1ab3d5eae135073c4
Gustavo Madeira Krieger <gustavo.krieger@cartesi.io>
2023-06-29 23:19:53 -0300

    refactor(graphql-server): standardize health check

--- offchain/graphql-server/src/lib.rs
+++ offchain/graphql-server/src/lib.rs
@@ -10,5 +10,47 @@
 // CONDITIONS OF ANY KIND, either express or implied. See the License for the
 // specific language governing permissions and limitations under the License.
 
+use snafu::ResultExt;
+
+pub use config::{CLIConfig, Config};
+pub use error::GraphQLServerError;
+pub use http::start_service;
+pub use schema::Context;
+
+pub mod config;
+mod error;
 pub mod http;
 pub mod schema;
+
+#[tracing::instrument(level = "trace", skip_all)]
+pub async fn run(config: Config) -> Result<(), GraphQLServerError> {
+    tracing::info!(?config, "starting graphql http service");
+
+    let repository =
+        rollups_data::Repository::new(config.graphql_config.repository_config)
+            .expect("failed to connect to database");
+    let context = Context::new(repository);
+    let service_handler = start_service(
+        &config.graphql_config.graphql_host,
+        config.graphql_config.graphql_port,
+        context,
+    )
+    .expect("failed to create server");
+
+    if config.health_check_config.healthcheck_disabled.is_none() {
+        let health_handle = http_health_check::start(
+            config.health_check_config.healthcheck_port,
+        );
+
+        tokio::select! {
+            ret = health_handle => {
+                ret.context(error::HealthCheckSnafu)
+            }
+            ret = service_handler => {
+                ret.context(error::ServerSnafu)
+            }
+        }
+    } else {
+        service_handler.await.context(error::ServerSnafu)
+    }
+}
