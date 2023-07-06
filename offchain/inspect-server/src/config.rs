e17b64dfaf13a939bcd8712397e54df198145216
Gabriel de Quadros Ligneul <8294320+gligneul@users.noreply.github.com>
2023-07-06 12:10:56 -0300

    fix(inspect-server): fix healthcheck config

--- offchain/inspect-server/src/config.rs
+++ offchain/inspect-server/src/config.rs
@@ -44,17 +44,21 @@ pub struct InspectServerConfig {
 
 #[derive(Debug, Clone, Parser)]
 pub struct InspectServerHealthCheckConfig {
-    /// Enable health check
-    #[arg(long = "no-inspect-server-healthcheck", env)]
-    pub healthcheck_disabled: Option<String>,
+    /// Enable or disable health check
+    #[arg(
+        long = "inspect-server-healthcheck-enabled",
+        env = "INSPECT_SERVER_HEALTHCHECK_ENABLED",
+        default_value_t = true
+    )]
+    pub enabled: bool,
 
     /// Port of health check
     #[arg(
         long = "inspect-server-healthcheck-port",
-        env,
-        default_value = "22025"
+        env = "INSPECT_SERVER_HEALTHCHECK_PORT",
+        default_value_t = 8080
     )]
-    pub healthcheck_port: u16,
+    pub port: u16,
 }
 
 /// Final config structure exported by this module
