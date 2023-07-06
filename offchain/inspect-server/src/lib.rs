1433d6b44a7a69557a674498bfaea9cb914a14fe
Gabriel de Quadros Ligneul <8294320+gligneul@users.noreply.github.com>
2023-07-06 12:10:56 -0300

    fix(inspect-server): fix healthcheck config

--- offchain/inspect-server/src/lib.rs
+++ offchain/inspect-server/src/lib.rs
@@ -30,10 +30,9 @@ pub async fn run(config: Config) -> Result<(), InspectError> {
         server::create(&config.inspect_server_config, inspect_client)
             .context(error::ServerSnafu)?;
 
-    if config.health_check_config.healthcheck_disabled.is_none() {
-        let health_handle = http_health_check::start(
-            config.health_check_config.healthcheck_port,
-        );
+    if config.health_check_config.enabled {
+        let health_handle =
+            http_health_check::start(config.health_check_config.port);
 
         tokio::select! {
             ret = health_handle => {
