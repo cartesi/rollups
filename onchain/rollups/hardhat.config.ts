1f8bcb44629f5354c4602a1a7fde61578310f545
Danilo Tuler <tuler@pobox.com>
2023-07-25 13:16:47 -0400

    feat: network configuration using @wagmi/chains

--- onchain/rollups/hardhat.config.ts
+++ onchain/rollups/hardhat.config.ts
@@ -12,6 +12,16 @@ import "hardhat-deploy";
 import "hardhat-abi-exporter";
 import "hardhat-gas-reporter";
 
+import {
+    Chain,
+    arbitrum,
+    arbitrumGoerli,
+    mainnet,
+    optimism,
+    optimismGoerli,
+    sepolia,
+} from "@wagmi/chains";
+
 // read MNEMONIC from env variable
 let mnemonic = process.env.MNEMONIC;
 
@@ -22,15 +32,19 @@ const ppath = (packageName: string, pathname: string) => {
     );
 };
 
-const infuraNetwork = (
-    network: string,
-    chainId?: number,
-    gas?: number
-): HttpNetworkUserConfig => {
+const networkConfig = (chain: Chain): HttpNetworkUserConfig => {
+    let url = chain.rpcUrls.public.http.at(0);
+
+    // support for infura and alchemy URLs through env variables
+    if (process.env.INFURA_ID && chain.rpcUrls.infura?.http) {
+        url = `${chain.rpcUrls.infura.http}/${process.env.INFURA_ID}`;
+    } else if (process.env.ALCHEMY_ID && chain.rpcUrls.alchemy?.http) {
+        url = `${chain.rpcUrls.alchemy.http}/${process.env.ALCHEMY_ID}`;
+    }
+
     return {
-        url: `https://${network}.infura.io/v3/${process.env.PROJECT_ID}`,
-        chainId,
-        gas,
+        chainId: chain.id,
+        url,
         accounts: mnemonic ? { mnemonic } : undefined,
     };
 };
@@ -42,28 +56,12 @@ const config: HardhatUserConfig = {
             url: process.env.RPC_URL || "http://localhost:8545",
             accounts: mnemonic ? { mnemonic } : undefined,
         },
-        mainnet: infuraNetwork("mainnet", 1, 6283185),
-        goerli: infuraNetwork("goerli", 5, 6283185),
-        sepolia: infuraNetwork("sepolia", 11155111, 6283185),
-        polygon_mumbai: infuraNetwork("polygon-mumbai", 80001),
-        arbitrum_goerli: infuraNetwork("arbitrum-goerli", 421613),
-        optimism_goerli: infuraNetwork("optimism-goerli", 420),
-        bsc_testnet: {
-            url: "https://data-seed-prebsc-1-s1.binance.org:8545",
-            chainId: 97,
-            accounts: mnemonic ? { mnemonic } : undefined,
-        },
-        iotex_testnet: {
-            url: "https://babel-api.testnet.iotex.io",
-            chainId: 4690,
-            accounts: mnemonic ? { mnemonic } : undefined,
-        },
-        chiado: {
-            url: "https://rpc.chiadochain.net",
-            chainId: 10200,
-            gasPrice: 1000000000,
-            accounts: mnemonic ? { mnemonic } : undefined,
-        },
+        arbitrum: networkConfig(arbitrum),
+        arbitrum_goerli: networkConfig(arbitrumGoerli),
+        mainnet: networkConfig(mainnet),
+        sepolia: networkConfig(sepolia),
+        optimism: networkConfig(optimism),
+        optimism_goerli: networkConfig(optimismGoerli),
     },
     solidity: {
         version: "0.8.19",
@@ -98,23 +96,16 @@ const config: HardhatUserConfig = {
         ],
         deployments: {
             localhost: ["deployments/localhost"],
-            mainnet: [ppath("@cartesi/util", "/deployments/mainnet")],
-            goerli: [ppath("@cartesi/util", "/deployments/goerli")],
-            sepolia: [ppath("@cartesi/util", "/deployments/sepolia")],
-            polygon_mumbai: [
-                ppath("@cartesi/util", "/deployments/polygon_mumbai"),
-            ],
+            arbitrum: [ppath("@cartesi/util", "/deployments/arbitrum")],
             arbitrum_goerli: [
                 ppath("@cartesi/util", "/deployments/arbitrum_goerli"),
             ],
+            mainnet: [ppath("@cartesi/util", "/deployments/mainnet")],
+            optimism: [ppath("@cartesi/util", "/deployments/optimism")],
             optimism_goerli: [
                 ppath("@cartesi/util", "/deployments/optimism_goerli"),
             ],
-            bsc_testnet: [ppath("@cartesi/util", "/deployments/bsc_testnet")],
-            iotex_testnet: [
-                ppath("@cartesi/util", "/deployments/iotex_testnet"),
-            ],
-            chiado: [ppath("@cartesi/util", "/deployments/chiado")],
+            sepolia: [ppath("@cartesi/util", "/deployments/sepolia")],
         },
     },
     namedAccounts: {
