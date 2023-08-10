6fb7b5c49a0e1dd8d93610a6f08623dbfa94b0c3
Gabriel Coutinho de Paula <gcdepaula@protonmail.com>
2023-08-10 08:53:17 +0200

    chore(arbitration): add license do sol files

--- onchain/permissionless-arbitration/src/tournament/abstracts/RootTournament.sol
+++ onchain/permissionless-arbitration/src/tournament/abstracts/RootTournament.sol
@@ -1,4 +1,6 @@
-// SPDX-License-Identifier: UNLICENSED
+// (c) Cartesi and individual authors (see AUTHORS)
+// SPDX-License-Identifier: Apache-2.0 (see LICENSE)
+
 pragma solidity ^0.8.17;
 
 import "./Tournament.sol";
