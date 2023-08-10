f63ce5b2d4a378676e6d44c4574b4a73c048a187
Gabriel Coutinho de Paula <gcdepaula@protonmail.com>
2023-08-10 14:27:51 +0200

    refactor(arbitration): improve solidity code quality

--- onchain/permissionless-arbitration/src/Commitment.sol
+++ onchain/permissionless-arbitration/src/Commitment.sol
@@ -10,27 +10,84 @@ import "./Machine.sol";
 // import "./Merkle.sol";
 
 library Commitment {
+    using Tree for Tree.Node;
     using Commitment for Tree.Node;
 
-    function proveFinalState(
-        Tree.Node root,
+    function requireState(
+        Tree.Node commitment,
+        uint64 level,
+        uint256 position,
+        Machine.Hash state,
+        bytes32[] calldata hashProof
+    ) internal pure {
+        uint64 treeHeight = ArbitrationConstants.height(level);
+        Tree.Node expectedCommitment = getRoot(
+            Machine.Hash.unwrap(state),
+            treeHeight,
+            position,
+            hashProof
+        );
+
+        require(commitment.eq(expectedCommitment), "commitment state doesn't match");
+    }
+
+
+    function isEven(uint256 x) private pure returns (bool) {
+        return x % 2 == 0;
+    }
+
+    function getRoot(
+        bytes32 leaf,
+        uint64 treeHeight,
+        uint256 position,
+        bytes32[] calldata siblings
+    ) internal pure returns (Tree.Node) {
+        uint nodesCount = treeHeight - 1;
+        assert(nodesCount == siblings.length);
+
+        for (uint i = 0; i < nodesCount; i++) {
+            if (isEven(position >> i)) {
+                leaf =
+                    keccak256(abi.encodePacked(leaf, siblings[i]));
+            } else {
+                leaf =
+                    keccak256(abi.encodePacked(siblings[i], leaf));
+            }
+        }
+
+        return Tree.Node.wrap(leaf);
+    }
+
+
+    function requireFinalState(
+        Tree.Node commitment,
         uint64 level,
         Machine.Hash finalState,
         bytes32[] calldata hashProof
     ) internal pure {
-        root.proveHash(
-            uint64(1 << ArbitrationConstants.height(level)),
-            finalState,
+        uint64 treeHeight = ArbitrationConstants.height(level);
+        Tree.Node expectedCommitment = getRootForLastLeaf(
+            treeHeight,
+            Machine.Hash.unwrap(finalState),
             hashProof
         );
+
+        require(commitment.eq(expectedCommitment), "commitment last state doesn't match");
     }
 
-    function proveHash(
-        Tree.Node root,
-        uint256 position,
-        Machine.Hash hash,
-        bytes32[] calldata hashProof
-    ) internal pure {
-        // TODO: call Merkle library
+
+    function getRootForLastLeaf(
+        uint64 treeHeight,
+        bytes32 leaf,
+        bytes32[] calldata siblings
+    ) internal pure returns (Tree.Node) {
+        uint nodesCount = treeHeight - 1;
+        assert(nodesCount == siblings.length);
+
+        for (uint i = 0; i < nodesCount; i++) {
+            leaf = keccak256(abi.encodePacked(siblings[i], leaf));
+        }
+
+        return Tree.Node.wrap(leaf);
     }
 }
--- onchain/permissionless-arbitration/src/Match.sol
+++ onchain/permissionless-arbitration/src/Match.sol
@@ -6,6 +6,7 @@ pragma solidity ^0.8.17;
 import "./CanonicalConstants.sol";
 import "./Tree.sol";
 import "./Machine.sol";
+import "./Commitment.sol";
 
 /// @notice Implements functionalities to advance a match, until the point where divergence is found.
 library Match {
@@ -13,6 +14,15 @@ library Match {
     using Match for Id;
     using Match for IdHash;
     using Match for State;
+    using Machine for Machine.Hash;
+    using Commitment for Tree.Node;
+
+    //
+    // Events
+    //
+
+    event matchAdvanced(Match.IdHash indexed, Tree.Node parent, Tree.Node left);
+
 
     //
     // Id
@@ -91,119 +101,71 @@ library Match {
         return (matchId.hashFromId(), state);
     }
 
-    function goDownLeftTree(
+    function advanceMatch(
         State storage state,
+        Id calldata id,
+        Tree.Node leftNode,
+        Tree.Node rightNode,
         Tree.Node newLeftNode,
         Tree.Node newRightNode
     ) internal {
-        assert(state.currentHeight > 1);
-        state.otherParent = state.leftNode;
-        state.leftNode = newLeftNode;
-        state.rightNode = newRightNode;
-
-        state.currentHeight--;
-    }
-
-    function goDownRightTree(
-        State storage state,
-        Tree.Node newLeftNode,
-        Tree.Node newRightNode
-    ) internal {
-        assert(state.currentHeight > 1);
-        state.otherParent = state.rightNode;
-        state.leftNode = newLeftNode;
-        state.rightNode = newRightNode;
-
-        state.runningLeafPosition += 1 << state.currentHeight; // TODO: verify
-        state.currentHeight--;
-    }
-
-    function setDivergenceOnLeftLeaf(
-        State storage state,
-        Tree.Node leftLeaf
-    )
-        internal
-        returns (Machine.Hash finalStateOne, Machine.Hash finalStateTwo)
-    {
-        assert(state.currentHeight == 1);
-        state.rightNode = leftLeaf;
-        state.currentHeight = 0;
-
-        if (state.height() % 2 == 0) {
-            finalStateOne = state.leftNode.toMachineHash();
-            finalStateTwo = state.rightNode.toMachineHash();
+        if (!state.agreesOnLeftNode(leftNode)) {
+            // go down left in Commitment tree
+            leftNode.requireChildren(newLeftNode, newRightNode);
+            state._goDownLeftTree(newLeftNode, newRightNode);
         } else {
-            finalStateOne = state.rightNode.toMachineHash();
-            finalStateTwo = state.leftNode.toMachineHash();
+            // go down right in Commitment tree
+            rightNode.requireChildren(newLeftNode, newRightNode);
+            state._goDownRightTree(newLeftNode, newRightNode);
         }
+
+        emit matchAdvanced(
+            id.hashFromId(),
+            state.otherParent,
+            state.leftNode
+        );
     }
 
-    function setDivergenceOnRightLeaf(
+    function sealMatch(
         State storage state,
-        Tree.Node rightLeaf
+        Id calldata id,
+        Machine.Hash initialState,
+        Tree.Node leftLeaf,
+        Tree.Node rightLeaf,
+        Machine.Hash agreeState,
+        bytes32[] calldata agreeStateProof
     )
         internal
-        returns (Machine.Hash finalStateOne, Machine.Hash finalStateTwo)
-    {
-        assert(state.currentHeight == 1);
-        state.leftNode = rightLeaf;
-        state.runningLeafPosition += 1; // TODO: verify
-        state.currentHeight = 0;
-
-        if (state.height() % 2 == 0) {
-            finalStateOne = state.rightNode.toMachineHash();
-            finalStateTwo = state.leftNode.toMachineHash();
-        } else {
-            finalStateOne = state.leftNode.toMachineHash();
-            finalStateTwo = state.rightNode.toMachineHash();
-        }
-    }
-
-    function getDivergence(
-        State storage state,
-        uint256 startCycle
-    )
-        internal
-        view
         returns (
-            Machine.Hash agreeHash,
-            uint256 agreeCycle,
-            Machine.Hash finalStateOne,
-            Machine.Hash finalStateTwo
+            Machine.Hash divergentStateOne,
+            Machine.Hash divergentStateTwo
         )
     {
-        assert(state.currentHeight == 0);
-        agreeHash = Machine.Hash.wrap(Tree.Node.unwrap(state.otherParent));
-        agreeCycle = state.toCycle(startCycle);
-
-        if (state.runningLeafPosition % 2 == 0) {
-            // divergence was set on left leaf
-            if (state.height() % 2 == 0) {
-                finalStateOne = state.leftNode.toMachineHash();
-                finalStateTwo = state.rightNode.toMachineHash();
-            } else {
-                finalStateOne = state.rightNode.toMachineHash();
-                finalStateTwo = state.leftNode.toMachineHash();
-            }
+        if (!state.agreesOnLeftNode(leftLeaf)) {
+            // Divergence is in the left leaf!
+            (divergentStateOne, divergentStateTwo) = state
+                ._setDivergenceOnLeftLeaf(leftLeaf);
         } else {
-            // divergence was set on right leaf
-            if (state.height() % 2 == 0) {
-                finalStateOne = state.rightNode.toMachineHash();
-                finalStateTwo = state.leftNode.toMachineHash();
-            } else {
-                finalStateOne = state.leftNode.toMachineHash();
-                finalStateTwo = state.rightNode.toMachineHash();
-            }
+            // Divergence is in the right leaf!
+            (divergentStateOne, divergentStateTwo) = state
+                ._setDivergenceOnRightLeaf(rightLeaf);
         }
+
+        // Prove initial hash is in commitment
+        if (state.runningLeafPosition == 0) {
+            require(agreeState.eq(initialState), "agree hash incorrect");
+        } else {
+            id.commitmentOne.requireState(
+                state.level,
+                state.runningLeafPosition - 1,
+                agreeState,
+                agreeStateProof
+            );
+        }
+
+        state._setAgreeState(agreeState);
     }
 
-    function setInitialState(
-        State storage state,
-        Machine.Hash initialState
-    ) internal {
-        assert(state.currentHeight == 0);
-        state.otherParent = Tree.Node.wrap(Machine.Hash.unwrap(initialState));
-    }
 
     //
     // View methods
@@ -246,6 +208,45 @@ library Match {
         return ArbitrationConstants.height(state.level);
     }
 
+    function getDivergence(
+        State memory state,
+        uint256 startCycle
+    )
+        internal
+        pure
+        returns (
+            Machine.Hash agreeHash,
+            uint256 agreeCycle,
+            Machine.Hash finalStateOne,
+            Machine.Hash finalStateTwo
+        )
+    {
+        assert(state.currentHeight == 0);
+        agreeHash = Machine.Hash.wrap(Tree.Node.unwrap(state.otherParent));
+        agreeCycle = state.toCycle(startCycle);
+
+        if (state.runningLeafPosition % 2 == 0) {
+            // divergence was set on left leaf
+            if (state.height() % 2 == 0) {
+                finalStateOne = state.leftNode.toMachineHash();
+                finalStateTwo = state.rightNode.toMachineHash();
+            } else {
+                finalStateOne = state.rightNode.toMachineHash();
+                finalStateTwo = state.leftNode.toMachineHash();
+            }
+        } else {
+            // divergence was set on right leaf
+            if (state.height() % 2 == 0) {
+                finalStateOne = state.rightNode.toMachineHash();
+                finalStateTwo = state.leftNode.toMachineHash();
+            } else {
+                finalStateOne = state.leftNode.toMachineHash();
+                finalStateTwo = state.rightNode.toMachineHash();
+            }
+        }
+    }
+
+
     //
     // Requires
     //
@@ -274,9 +275,86 @@ library Match {
         state.otherParent.requireChildren(leftNode, rightNode);
     }
 
+
     //
     // Private
     //
+
+    function _goDownLeftTree(
+        State storage state,
+        Tree.Node newLeftNode,
+        Tree.Node newRightNode
+    ) internal {
+        assert(state.currentHeight > 1);
+        state.otherParent = state.leftNode;
+        state.leftNode = newLeftNode;
+        state.rightNode = newRightNode;
+
+        state.currentHeight--;
+    }
+
+    function _goDownRightTree(
+        State storage state,
+        Tree.Node newLeftNode,
+        Tree.Node newRightNode
+    ) internal {
+        assert(state.currentHeight > 1);
+        state.otherParent = state.rightNode;
+        state.leftNode = newLeftNode;
+        state.rightNode = newRightNode;
+
+        state.runningLeafPosition += 1 << state.currentHeight;
+        state.currentHeight--;
+    }
+
+    function _setDivergenceOnLeftLeaf(
+        State storage state,
+        Tree.Node leftLeaf
+    )
+        internal
+        returns (Machine.Hash finalStateOne, Machine.Hash finalStateTwo)
+    {
+        assert(state.currentHeight == 1);
+        state.rightNode = leftLeaf;
+        state.currentHeight = 0;
+
+        if (state.height() % 2 == 0) {
+            finalStateOne = state.leftNode.toMachineHash();
+            finalStateTwo = state.rightNode.toMachineHash();
+        } else {
+            finalStateOne = state.rightNode.toMachineHash();
+            finalStateTwo = state.leftNode.toMachineHash();
+        }
+    }
+
+    function _setDivergenceOnRightLeaf(
+        State storage state,
+        Tree.Node rightLeaf
+    )
+        internal
+        returns (Machine.Hash finalStateOne, Machine.Hash finalStateTwo)
+    {
+        assert(state.currentHeight == 1);
+        state.leftNode = rightLeaf;
+        state.runningLeafPosition += 1;
+        state.currentHeight = 0;
+
+        if (state.height() % 2 == 0) {
+            finalStateOne = state.rightNode.toMachineHash();
+            finalStateTwo = state.leftNode.toMachineHash();
+        } else {
+            finalStateOne = state.leftNode.toMachineHash();
+            finalStateTwo = state.rightNode.toMachineHash();
+        }
+    }
+
+    function _setAgreeState(
+        State storage state,
+        Machine.Hash initialState
+    ) internal {
+        assert(state.currentHeight == 0);
+        state.otherParent = Tree.Node.wrap(Machine.Hash.unwrap(initialState));
+    }
 
     function _toCycle(
         State memory state,
@@ -285,6 +363,6 @@ library Match {
     ) internal pure returns (uint256) {
         uint256 step = 1 << log2step;
         uint256 leafPosition = state.runningLeafPosition;
-        return base + (leafPosition * step); // TODO verify
+        return base + (leafPosition * step);
     }
 }
