96393c9ac134bff9977fb833b6132b751ebad7e8
Gabriel Coutinho de Paula <gcdepaula@protonmail.com>
2023-08-21 20:52:38 +0200

    chore(arbitration): update machine version

--- onchain/permissionless-arbitration/offchain/README.md
+++
@@ -1,7 +0,0 @@
-# Permissionless Arbitration (NxN) Lua prototype Node
-
-## Run example
-
-```
-docker build -t nxn_playground:latest . && docker run --platform linux/amd64 --rm nxn_playground:latest
-```
--- onchain/permissionless-arbitration/offchain/program/gen_machine_simple.sh
+++ onchain/permissionless-arbitration/offchain/program/gen_machine_simple.sh
@@ -3,7 +3,7 @@ cartesi-machine \
   --no-root-flash-drive \
   --rom-image="./bins/bootstrap.bin" \
   --ram-image="./bins/rv64ui-p-addi.bin" \
-  --uarch-ram-image="/opt/cartesi/share/images/uarch-ram.bin" \
+  --uarch-ram-image="/usr/share/cartesi-machine/uarch/uarch-ram.bin" \
   --uarch-ram-length=0x1000000 \
   --max-mcycle=0 \
   --store="simple-program"
